48fde55f9dfc24739e9dccc0700688cdd766e02d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-01-08 19:06:27 +0100

    Added afx defines

--- src/StdAfx.h
+++ src/StdAfx.h
@@ -49,6 +49,13 @@
 #define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS
 #define _AFX_ALL_WARNINGS
 
+#define _AFX_PORTABLE
+#define _FORCENAMELESSUNION
+#define _AFX_NO_DAO_SUPPORT
+#define _AFX_NO_DHTML_SUPPORT
+#define _AFX_NO_OLEDB_SUPPORT
+#define _AFX_NO_RICHEDIT_SUPPORT
+
 #include <afxwin.h>
 #include <afxext.h>
 
