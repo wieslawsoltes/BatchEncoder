b5f514036da96ec9be99e03516ca05a48e00ec1b
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-01-10 10:11:30 +0100

    Update version.h

--- src/version.h
+++ src/version.h
@@ -1,3 +1,6 @@
+// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 #define VERSION_MAJOR               1
 #define VERSION_MINOR               0
 #define VERSION_REVISION            0
