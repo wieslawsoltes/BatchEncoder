23461448543ca4e55ebb83206c719d4c0710ea53
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-01-11 12:15:03 +0100

    Update Configuration.h
    [skip ci]

--- src/Configuration.h
+++ src/Configuration.h
@@ -6,10 +6,6 @@
 #include "afxwin.h"
 #include "afxcmn.h"
 
-//
-// CONSTANTS
-//
-
 // number of supported input file extension
 #define NUM_INPUT_EXT 19
 
@@ -127,118 +123,6 @@ enum TOOL_ID
     _T("TTA (*.tta)|*.tta|") \
     _T("TAK (*.tak)|*.tak||")
 
-// number of browse paths to load/save:
-//
-// CString szBrowsePath[n];
-// 
-// n   Dialog           Action
-// --  ---------------  ------------------
-//  0  Main             Load list...
-//  1  Main             Save list...
-//  -  Main             Create batch-file...
-//  2  Main             Add Files
-//  3  Main             Add Directory
-//  4  Main             ... (browse for outpath)
-//  5  Advanced         ... (browse for logfile)
-//  6  Formats          Load
-//  7  Formats          Save As
-//
-//  8  Formats          ... (browse for tool #00)
-//  9  Formats          ... (browse for tool #01)
-// 10  Formats          ... (browse for tool #02)
-// 11  Formats          ... (browse for tool #03)
-// 12  Formats          ... (browse for tool #04)
-// 13  Formats          ... (browse for tool #05)
-// 14  Formats          ... (browse for tool #06)
-// 15  Formats          ... (browse for tool #07)
-// 16  Formats          ... (browse for tool #08)
-// 17  Formats          ... (browse for tool #09)
-// 18  Formats          ... (browse for tool #10)
-// 19  Formats          ... (browse for tool #11)
-// 20  Formats          ... (browse for tool #12)
-// 21  Formats          ... (browse for tool #13)
-// 22  Formats          ... (browse for tool #14)
-// 23  Formats          ... (browse for tool #15)
-// 24  Formats          ... (browse for tool #16)
-// 25  Formats          ... (browse for tool #17)
-// 26  Formats          ... (browse for tool #18)
-// 27  Formats          ... (browse for tool #19)
-// 28  Formats          ... (browse for tool #20)
-// 29  Formats          ... (browse for tool #21)
-// 30  Formats          ... (browse for tool #22)
-// 31  Formats          ... (browse for tool #23)
-// 32  Formats          ... (browse for tool #24)
-// 33  Formats          ... (browse for tool #25)
-// 34  Formats          ... (browse for tool #26)
-// 35  Formats          ... (browse for tool #27)
-//
-// 36  Progress         ... (browse for function #00)
-// 37  Progress         ... (browse for function #01)
-// 38  Progress         ... (browse for function #02)
-// 39  Progress         ... (browse for function #03)
-// 40  Progress         ... (browse for function #04)
-// 41  Progress         ... (browse for function #05)
-// 42  Progress         ... (browse for function #06)
-// 43  Progress         ... (browse for function #07)
-// 44  Progress         ... (browse for function #08)
-// 45  Progress         ... (browse for function #09)
-// 46  Progress         ... (browse for function #10)
-// 47  Progress         ... (browse for function #11)
-// 48  Progress         ... (browse for function #12)
-// 49  Progress         ... (browse for function #13)
-// 50  Progress         ... (browse for function #14)
-// 51  Progress         ... (browse for function #15)
-// 52  Progress         ... (browse for function #16)
-// 53  Progress         ... (browse for function #17)
-// 54  Progress         ... (browse for function #18)
-// 55  Progress         ... (browse for function #19)
-// 56  Progress         ... (browse for function #20)
-// 57  Progress         ... (browse for function #21)
-// 58  Progress         ... (browse for function #22)
-// 59  Progress         ... (browse for function #23)
-// 60  Progress         ... (browse for function #24)
-// 61  Progress         ... (browse for function #25)
-// 62  Progress         ... (browse for function #26)
-// 63  Progress         ... (browse for function #27)
-//
-// 64  Presets          Load #00
-// 65  Presets          Save As #00
-// 66  Presets          Load #01
-// 67  Presets          Save As #01
-// 68  Presets          Load #02
-// 69  Presets          Save As #02
-// 70  Presets          Load #03
-// 71  Presets          Save As #03
-// 72  Presets          Load #04
-// 73  Presets          Save As #04
-// 74  Presets          Load #05
-// 75  Presets          Save As #05
-// 76  Presets          Load #06
-// 77  Presets          Save As #06
-// 78  Presets          Load #07
-// 79  Presets          Save As #07
-// 80  Presets          Load #08
-// 81  Presets          Save As #08
-// 82  Presets          Load #09
-// 83  Presets          Save As #09
-// 84  Presets          Load #10
-// 85  Presets          Save As #10
-// 86  Presets          Load #11
-// 87  Presets          Save As #11
-#define NUM_BROWSE_PATH 88
-
-#define NUM_BROWSE_PATH_ADVANCED 1
-#define START_BROWSE_PATH_ADVANCED 5
-
-#define NUM_BROWSE_PATH_FORMATS 30
-#define START_BROWSE_PATH_FORMATS 6
-
-#define NUM_BROWSE_PATH_PROGRESS 30
-#define START_BROWSE_PATH_PROGRESS 36
-
-#define NUM_BROWSE_PATH_PRESETS 24
-#define START_BROWSE_PATH_PRESETS 64
-
 // number of supported thread priorities
 #define NUM_THREAD_PRIORITY 7
 
@@ -270,15 +154,8 @@ extern const bool g_bDefaultInPipes[NUM_FORMAT_NAMES];
 extern const bool g_bDefaultOutPipes[NUM_FORMAT_NAMES];
 extern const TCHAR *g_bDefaultFunction[];
 
-//
-// LISTS
-
 #include "LListFiles.h"
 #include "LListPresets.h"
-
-//
-// CONFIGURATION
-//
 
 class CSettings
 {
@@ -339,12 +216,6 @@ public:
     CString szPresetsFile[NUM_PRESET_FILES];
 };
 
-class CBrowse
-{
-public:
-    CString szBrowsePath[NUM_BROWSE_PATH];
-};
-
 class CFiles
 {
 public:
@@ -358,6 +229,5 @@ public:
     CColors m_Colors;
     CFormats m_Formats;
     CPresets m_Presets;
-    CBrowse m_Browse;
     CFiles m_Files;
 };
