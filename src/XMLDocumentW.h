b008c7110f7837957a574d6b169f761045b57caf
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-01-11 13:06:48 +0100

    Update XMLDocumentW.h
    [skip ci]

--- src/XMLDocumentW.h
+++ src/XMLDocumentW.h
@@ -1,11 +1,9 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-// TinyXml2 https://github.com/leethomason/tinyxml2
-
 #pragma once
 
-#include "xml\tinyxml2.h"
+#include "xml\tinyxml2.h" // https://github.com/leethomason/tinyxml2
 
 class CXMLDocumentW : public tinyxml2::XMLDocument
 {
