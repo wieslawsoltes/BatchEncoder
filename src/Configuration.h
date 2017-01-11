517f8c0a6a7a23d0cbae132d4b5cec445dbde6db
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-01-11 08:58:10 +0100

    Update Configuration.h

--- src/Configuration.h
+++ src/Configuration.h
@@ -290,14 +290,14 @@ public:
     CString szLogFileName;
     int nLogEncoding;
 public:
-    void Copy(const CSettings &other)
+    void Copy(CSettings& other)
     {
-        other->nThreadPriorityIndex = this.nThreadPriorityIndex;
-        other->nProcessPriorityIndex = this.nProcessPriorityIndex;
-        other->bDeleteOnError = this.bDeleteOnError;
-        other->bStopOnErrors = this.bStopOnErrors;
-        other->szLogFileName = this.szLogFileName;
-        other->nLogEncoding = this.nLogEncoding;
+        other.nThreadPriorityIndex = this.nThreadPriorityIndex;
+        other.nProcessPriorityIndex = this.nProcessPriorityIndex;
+        other.bDeleteOnError = this.bDeleteOnError;
+        other.bStopOnErrors = this.bStopOnErrors;
+        other.szLogFileName = this.szLogFileName;
+        other.nLogEncoding = this.nLogEncoding;
     }
 };
 
@@ -316,13 +316,13 @@ public:
     bool bOutput;
     CString szFunction;
 public:
-    void Copy(const CFormat &other)
+    void Copy(CFormat& other)
     {
-        other->szTemplate = this.szTemplate;
-        other->szPath = this..szPath;
-        other->bInput = this..bInput;
-        other->bOutput = this..bOutput;
-        other->szFunction = this..szFunction;
+        other.szTemplate = this.szTemplate;
+        other.szPath = this..szPath;
+        other.bInput = this..bInput;
+        other.bOutput = this..bOutput;
+        other.szFunction = this..szFunction;
     }
 };
 
