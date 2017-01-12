0eefea8a95b2239f408a9bc62b9e4fad2c6385d5
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-01-12 12:50:55 +0100

    Update NewItemData.h
    [skip ci]

--- src/NewItemData.h
+++ src/NewItemData.h
@@ -3,6 +3,14 @@
 
 #pragma once
 
+// nAction:
+// 0 - Add new item to memory ItemsList.
+// 1 - Add new item to control ItemsList.
+// 2 - Add new item to memory and control ItemsList.
+#define ADD_ITEM_MEMORY             0
+#define ADD_ITEM_CONTROL            1
+#define ADD_ITEM_MEMORY_AND_CONTROL 2
+
 typedef struct TNewItemData
 {
     int nAction;
