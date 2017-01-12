ed577bed3a576bf1f2a53ebb7c517cb8f39a0498
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-01-12 13:05:44 +0100

    Update Configuration.h
    [skip ci]

--- src/Configuration.h
+++ src/Configuration.h
@@ -262,3 +262,24 @@ public:
     CString szPresetsFile[NUM_PRESET_FILES];
     CItemsList m_Items;
 };
+
+// nAction
+#define ADD_ITEM_NONE              -1
+#define ADD_ITEM_MEMORY             0
+#define ADD_ITEM_CONTROL            1
+#define ADD_ITEM_MEMORY_AND_CONTROL 2
+
+typedef struct TNewItemData
+{
+    int nAction;
+    CString szFileName;
+    int nItem;
+    CString szName;
+    CString szOutExt;
+    int nPreset;
+    BOOL bCheck;
+    CString szTime;
+    CString szStatus;
+} NewItemData, *PNewItemData;
+
+void InitNewItemData(NewItemData &nid);
