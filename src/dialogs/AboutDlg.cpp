3852c5722700c30af736942adcf0793d0ef9d841
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-01-13 13:11:23 +0100

    Update AboutDlg.cpp
    [skip ci]

--- src/dialogs/AboutDlg.cpp
+++ src/dialogs/AboutDlg.cpp
@@ -1,9 +1,9 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#include "StdAfx.h"
-#include "BatchEncoder.h"
-#include "Utilities.h"
+#include "..\StdAfx.h"
+#include "..\BatchEncoder.h"
+#include "..\utilities\Utilities.h"
 #include "AboutDlg.h"
 #include ".\aboutdlg.h"
 
