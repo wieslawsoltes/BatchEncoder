f14a5c8ac784a26bc840a8f096fc94afb7c9d364
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-01-13 13:03:48 +0100

    Create Controls.h
    [skip ci]

---
+++ src/controls/Controls.h
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#pragma once
+
+#include "Hyperlink.h"
+#include "ItemListCtrl.h"
+#include "MyButton.h"
+#include "MyComboBox.h"
+#include "MyStatic.h"
+#include "MyStatusBarCtrl.h"
+#include "ResizeDialog.h"
