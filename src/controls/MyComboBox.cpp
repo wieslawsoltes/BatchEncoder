51cb972fb34744b678e06bfd2553be7d63791b1f
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-01-13 13:16:07 +0100

    Update MyComboBox.cpp
    [skip ci]

--- src/controls/MyComboBox.cpp
+++ src/controls/MyComboBox.cpp
@@ -1,10 +1,9 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#include "StdAfx.h"
-#include "BatchEncoder.h"
+#include "..\StdAfx.h"
+#include "..\BatchEncoder.h"
 #include "MyComboBox.h"
-#include ".\mycombobox.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
