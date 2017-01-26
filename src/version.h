bb0b6d36126a5098c8a211d6672a32f637780df5
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-01-26 14:25:42 +0100

    Update version.h
    [skip ci]

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
-#define VERSION_MAJOR               1
-#define VERSION_MINOR               0
+#define VERSION_MAJOR               0
+#define VERSION_MINOR               1
 #define VERSION_REVISION            0
 #define VERSION_BUILD               0
 
