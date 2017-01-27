64e9fc5fcf556bd099b26e9be47e62e869e4a8c0
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-01-27 16:35:24 +0100

    Update XmlConfiguration.h

--- src/XmlConfiguration.h
+++ src/XmlConfiguration.h
@@ -15,25 +15,25 @@ public:
     virtual ~XmlConfiguration();
 private:
     CString ToCString(const char *pszUtf8);
-public:
     void GetOptions(tinyxml2::XMLElement *pOptionsElem, COptions &m_Options);
     void SetOptions(tinyxml2::XMLElement *pOptionsElem, COptions &m_Options);
-    void GetOptions(COptions &m_Options);
-    void SetOptions(COptions &m_Options);
     void GetPresets(tinyxml2::XMLElement *pPresetsElem, CPresetsList &m_Presets);
     void SetPresets(tinyxml2::XMLElement *pPresetsElem, CPresetsList &m_Presets);
-    void GetPresets(CPresetsList &m_Presets);
-    void SetPresets(CPresetsList &m_Presets);
     void GetFormat(tinyxml2::XMLElement *pFormatElem, CFormat &m_Format);
     void SetFormat(tinyxml2::XMLElement *pFormatElem, CFormat &m_Format);
-    void GetFormat(CFormat &m_Format);
-    void SetFormat(CFormat &m_Format);
     void GetFormats(tinyxml2::XMLElement *pFormatsElem, CFormatsList &m_Formats);
     void SetFormats(tinyxml2::XMLElement *pFormatsElem, CFormatsList &m_Formats);
-    void GetFormats(CFormatsList &m_Formats);
-    void SetFormats(CFormatsList &m_Formats);
     void GetItems(tinyxml2::XMLElement *pItemsElem, CItemsList &m_Items);
     void SetItems(tinyxml2::XMLElement *pItemsElem, CItemsList &m_Items);
+public:
+    void GetOptions(COptions &m_Options);
+    void SetOptions(COptions &m_Options);
+    void GetPresets(CPresetsList &m_Presets);
+    void SetPresets(CPresetsList &m_Presets);
+    void GetFormat(CFormat &m_Format);
+    void SetFormat(CFormat &m_Format);
+    void GetFormats(CFormatsList &m_Formats);
+    void SetFormats(CFormatsList &m_Formats);
     void GetItems(CItemsList &m_Items);
     void SetItems(CItemsList &m_Items);
     bool Open(CString szFileName);
