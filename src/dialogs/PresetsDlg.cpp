40a1e86d133998b0e99c94fad95518754a41002a
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-01-27 16:25:31 +0100

    Update PresetsDlg.cpp

--- src/dialogs/PresetsDlg.cpp
+++ src/dialogs/PresetsDlg.cpp
@@ -3,6 +3,7 @@
 
 #include "StdAfx.h"
 #include "..\BatchEncoder.h"
+#include "..\XmlConfiguration.h"
 #include "..\utilities\Utilities.h"
 #include "..\utilities\UnicodeUtf8.h"
 #include "..\utilities\Utf8String.h"
