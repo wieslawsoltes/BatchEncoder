7278ab66386188064019fcd7f7ee45c18a68f38c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-02-02 23:32:12 +0100

    Removed unused code

--- src/utilities/TimeCount.h
+++ src/utilities/TimeCount.h
@@ -8,24 +8,24 @@ class CTimeCount
 private:
     LARGE_INTEGER f[3];
 public:
-    CTimeCount(void)
+    CTimeCount()
     {
     };
-    virtual ~CTimeCount(void)
+    virtual ~CTimeCount()
     {
     };
 public:
-    void Start(void)
+    void Start()
     {
         ZeroMemory(this->f, (3 * sizeof(LARGE_INTEGER)));
         ::QueryPerformanceFrequency(&this->f[0]);
         ::QueryPerformanceCounter(&this->f[1]);
     }
-    void Stop(void)
+    void Stop()
     {
         ::QueryPerformanceCounter(&this->f[2]);
     }
-    double ElapsedTime(void)
+    double ElapsedTime()
     {
         return (double)((this->f[2].QuadPart - this->f[1].QuadPart) / (double)this->f[0].QuadPart);
     }
