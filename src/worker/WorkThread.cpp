dbb7fcb48909a3b433b9ced06a8874846e7fcd88
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-02-05 17:35:08 +0100

    Use strings

--- src/worker/WorkThread.cpp
+++ src/worker/WorkThread.cpp
@@ -3,57 +3,11 @@
 
 #include "StdAfx.h"
 #include "..\BatchEncoder.h"
+#include "..\Strings.h"
 #include "..\utilities\TimeCount.h"
 #include "..\utilities\Utilities.h"
 #include "..\Configuration.h"
 #include "WorkThread.h"
-
-const TCHAR* pszDefaulTime = _T("--:--");
-
-const TCHAR* pszProgresssLoop[] = {
-    /* 00 */ _T("Error: can not load GetProgress function library dll."),
-    /* 01 */ _T("Error: can not get GetProgress function address."),
-    /* 02 */ _T("Error: console line is too large for read buffer.")
-};
-
-const TCHAR* pszConvertConsole[] = {
-    /* 00 */ _T("Error: invalid format pipe configuration."),
-    /* 01 */ _T("Error: can not create pipes for stderr."),
-    /* 02 */ _T("Error: can not duplicate stderr pipe to prevent child process from closing the pipe."),
-    /* 03 */ _T("Error: can not create command-line process (%d)."),
-    /* 04 */ _T("Error: progress did not reach 100%."),
-    /* 05 */ _T("Done")
-};
-
-const TCHAR* pszConvertPipes[] = {
-    /* 00 */ _T("Error: invalid format pipe configuration."),
-    /* 01 */ _T("Error: can not create pipes for stdin."),
-    /* 02 */ _T("Error: can not set stdin pipe inherit flag."),
-    /* 03 */ _T("Error: can not create pipes for stdout."),
-    /* 04 */ _T("Error: can not set stdout pipe inherit flag."),
-    /* 05 */ _T("Error: can not create command-line process (%d)."),
-    /* 06 */ _T("Error: can not create read thread."),
-    /* 07 */ _T("Error: can not create write thread."),
-    /* 08 */ _T("Error: progress did not reach 100%."),
-    /* 09 */ _T("Done")
-};
-
-const TCHAR* pszConvertItem[] = {
-    /* 00 */ _T("Error: can not find input file."),
-    /* 01 */ _T("Error: can not find valid encoder by id."),
-    /* 02 */ _T("Error: can not find encoder format preset."),
-    /* 03 */ _T("Error: can not find valid decoder by extension."),
-    /* 04 */ _T("Error: can not find decoder format preset."),
-    /* 05 */ _T("Error: decoder output not supported by encoder."),
-    /* 06 */ _T("Decoding..."),
-    /* 07 */ _T("Error: can not find decoded file."),
-    /* 08 */ _T("Error: exception thrown while converting file."),
-    /* 09 */ _T("Encoding..."),
-    /* 10 */ _T("Decoding..."),
-    /* 11 */ _T("Processing..."),
-    /* 12 */ _T("Error: can not find encoded file."),
-    /* 13 */ _T("Error: exception thrown while converting file.")
-};
 
 bool ProgresssLoop(CFileContext* pContext, CProcessContext &processContext, int &nProgress)
 {
