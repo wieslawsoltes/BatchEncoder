7c52077728c8dad097d5074b1ae23ef3fc8e8ce4
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-02-20 13:48:52 +0100

    Update LanguagesList.h

--- src/configuration/LanguagesList.h
+++ src/configuration/LanguagesList.h
@@ -4,108 +4,11 @@
 #pragma once
 
 #include <afxstr.h>
-#include <afxtempl.h>
+#include "ListT.h"
 #include "Language.h"
 
-class CLanguagesList
+class CLanguagesList : public CListT<CLanguage>
 {
-public:
-    CList<CLanguage, const CLanguage&> m_Languages;
-public:
-    void SetData(CLanguage& language, int idx)
-    {
-        m_Languages.SetAt(m_Languages.FindIndex(idx), language);
-    }
-    CLanguage& GetData(int idx)
-    {
-        return m_Languages.GetAt(m_Languages.FindIndex(idx));
-    }
-public:
-    CLanguagesList()
-    {
-    }
-    CLanguagesList(const CLanguagesList &other)
-    {
-        Copy(other);
-    }
-    CLanguagesList& operator=(const CLanguagesList &other)
-    {
-        Copy(other);
-        return *this;
-    }
-    virtual ~CLanguagesList()
-    {
-        if (m_Languages.GetCount() != 0)
-            m_Languages.RemoveAll();
-    }
-public:
-    void Copy(const CLanguagesList &other)
-    {
-        this->RemoveAllNodes();
-        int nLanguages = (int)other.m_Languages.GetCount();
-        for (int i = 0; i < nLanguages; i++)
-        {
-            CLanguage language = other.m_Languages.GetAt(other.m_Languages.FindIndex(i));
-            this->InsertNode(language);
-        }
-    }
-public:
-    bool IsEmpty()
-    {
-        return (m_Languages.GetCount() == 0) ? true : false;
-    }
-    int GetSize()
-    {
-        return (int)m_Languages.GetCount();
-    }
-public:
-    void InsertNode(CLanguage &language)
-    {
-        m_Languages.AddTail(language);
-    }
-    void InsertBefore(CLanguage &language, int nIndex)
-    {
-        POSITION pos = m_Languages.FindIndex(nIndex);
-        if (pos != NULL)
-            m_Languages.InsertBefore(pos, language);
-    }
-    void InsertAfter(CLanguage &language, int nIndex)
-    {
-        POSITION pos = m_Languages.FindIndex(nIndex);
-        if (pos != NULL)
-            m_Languages.InsertAfter(pos, language);
-    }
-public:
-    void RemoveNode(int pstn = -1)
-    {
-        m_Languages.RemoveAt(m_Languages.FindIndex(pstn));
-    }
-    void RemoveAllNodes(void)
-    {
-        if (m_Languages.GetCount() != 0)
-            m_Languages.RemoveAll();
-    }
-public:
-    void Copy(CLanguagesList& other)
-    {
-        int nLanguages = this->GetSize();
-        for (int i = 0; i < nLanguages; i++)
-        {
-            CLanguage& language = this->GetData(i);
-            CLanguage copy = language;
-            other.InsertNode(copy);
-        }
-    }
-public:
-    void SwapItems(int idx1, int idx2)
-    {
-        if ((idx1 < 0) || (idx2 < 0) || (idx1 >= GetSize()) || (idx2 >= GetSize()))
-            return;
-        CLanguage language1 = this->GetData(idx1);
-        CLanguage language2 = this->GetData(idx2);
-        this->SetData(language1, idx2);
-        this->SetData(language2, idx1);
-    }
 public:
     int GetLanguageById(CString szLanguageId)
     {
