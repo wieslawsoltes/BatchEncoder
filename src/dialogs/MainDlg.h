ee6d35bf77687729ce060cbe3dd520b6927c32e1
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-02-23 13:27:27 +0100

    Update MainDlg.h
    [skip ci]

--- src/dialogs/MainDlg.h
+++ src/dialogs/MainDlg.h
@@ -126,6 +126,7 @@ public:
     afx_msg void OnOptionsEnsureVisible();
     afx_msg void OnOptionsFindDecoder();
     afx_msg void OnOptionsValidateFiles();
+    afx_msg void OnOptionsOverwriteFiles();
     afx_msg void OnLanguageDefault();
     afx_msg void OnLanguageChange(UINT nID);
     afx_msg void OnHelpWebsite();
