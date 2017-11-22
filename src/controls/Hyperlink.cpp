0505adaac0681feb4eb2294dc1299312e7b3759b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-11-22 09:58:36 +0100

    Updated controls

--- src/controls/Hyperlink.cpp
+++ src/controls/Hyperlink.cpp
@@ -2,10 +2,10 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include "StdAfx.h"
-#include "MainApp.h"
 #include "Hyperlink.h"
 
 IMPLEMENT_DYNAMIC(CHyperlink, CStatic)
+
 CHyperlink::CHyperlink()
 {
     m_hCursor = NULL;
--- src/controls/MyEdit.cpp
+++ src/controls/MyEdit.cpp
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include "StdAfx.h"
-#include "MainApp.h"
 #include "MyEdit.h"
 
 IMPLEMENT_DYNAMIC(CMyEdit, CEdit)
