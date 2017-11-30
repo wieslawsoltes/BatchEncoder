d3facac9ed6524861dc6bc76667dcb06c0e59b3f
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2017-11-30 14:59:33 +0100

    Create ToolsDlg.cpp

---
+++ src/dialogs/ToolsDlg.cpp
@@ -0,0 +1,86 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include "StdAfx.h"
+#include "MainApp.h"
+#include "Strings.h"
+#include "ToolsDlg.h"
+
+IMPLEMENT_DYNAMIC(CToolsDlg, CDialog)
+CToolsDlg::CToolsDlg(CWnd* pParent /*=NULL*/)
+    : CMyDialogEx(CToolsDlg::IDD, pParent)
+{
+    this->m_hIcon = AfxGetApp()->LoadIcon(IDI_ICON_MAIN);
+}
+
+CToolsDlg::~CToolsDlg()
+{
+
+}
+
+void CToolsDlg::DoDataExchange(CDataExchange* pDX)
+{
+    CMyDialogEx::DoDataExchange(pDX);
+    DDX_Control(pDX, IDOK, m_BtnOK);
+    DDX_Control(pDX, IDCANCEL, m_BtnCancel);
+}
+
+BEGIN_MESSAGE_MAP(CToolsDlg, CMyDialogEx)
+    ON_WM_PAINT()
+    ON_WM_QUERYDRAGICON()
+    ON_BN_CLICKED(IDOK, OnBnClickedOk)
+    ON_BN_CLICKED(IDCANCEL, OnBnClickedCancel)
+    ON_WM_CLOSE()
+END_MESSAGE_MAP()
+
+BOOL CToolsDlg::OnInitDialog()
+{
+    CMyDialogEx::OnInitDialog();
+
+    InitCommonControls();
+
+    SetIcon(m_hIcon, TRUE);
+    SetIcon(m_hIcon, FALSE);
+
+    return TRUE;
+}
+
+void CToolsDlg::OnPaint()
+{
+    if (IsIconic())
+    {
+        CPaintDC dc(this);
+        SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+        int cxIcon = GetSystemMetrics(SM_CXICON);
+        int cyIcon = GetSystemMetrics(SM_CYICON);
+        CRect rect;
+        GetClientRect(&rect);
+        int x = (rect.Width() - cxIcon + 1) / 2;
+        int y = (rect.Height() - cyIcon + 1) / 2;
+        dc.DrawIcon(x, y, m_hIcon);
+    }
+    else
+    {
+        CMyDialogEx::OnPaint();
+    }
+}
+
+HCURSOR CToolsDlg::OnQueryDragIcon()
+{
+    return static_cast<HCURSOR>(m_hIcon);
+}
+
+void CToolsDlg::OnBnClickedOk()
+{
+    OnOK();
+}
+
+void CToolsDlg::OnBnClickedCancel()
+{
+    OnCancel();
+}
+
+void CToolsDlg::OnClose()
+{
+    CMyDialogEx::OnClose();
+}
