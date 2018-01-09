a8dea79792a1ee445901d3f54d140c6a14c0c53a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-01-09 19:18:28 +0100

    Updated rewavi tool

--- scripts/download.ps1
+++ scripts/download.ps1
@@ -84,8 +84,8 @@ $Tools =
     ( "https://github.com/wieslawsoltes/AvsDec/releases/download/2.2/AvsDec-2.2-Win32-UNICODE.zip", "AvsDec-2.2-Win32-UNICODE.zip" ),
     ( "https://github.com/wieslawsoltes/AvsDec/releases/download/2.2/AvsDec-2.2-x64-UNICODE.zip", "AvsDec-2.2-x64-UNICODE.zip" ),
     # rewavi
-    ( "https://github.com/wieslawsoltes/rewavi/releases/download/0.05/rewavi-0.05-Win32.zip", "rewavi-0.05-Win32.zip" ),
-    ( "https://github.com/wieslawsoltes/rewavi/releases/download/0.05/rewavi-0.05-x64.zip", "rewavi-0.05-x64.zip" )
+    ( "https://github.com/wieslawsoltes/rewavi/releases/download/0.06/rewavi-0.06-Win32.zip", "rewavi-0.06-Win32.zip" ),
+    ( "https://github.com/wieslawsoltes/rewavi/releases/download/0.06/rewavi-0.06-x64.zip", "rewavi-0.06-x64.zip" )
 )
 
 if(!(Test-Path -Path $Path )) {
