903abc723b2cbb1235caf7111ed8ae8dd61b4af6
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2018-01-09 10:10:19 +0100

    Update download.ps1

--- scripts/download.ps1
+++ scripts/download.ps1
@@ -81,8 +81,8 @@ $Tools =
     # NeroAACCodec
     ( "http://ftp6.nero.com/tools/NeroAACCodec-1.5.1.zip", "NeroAACCodec-1.5.1.zip" ),
     # AvsDec
-    ( "https://github.com/wieslawsoltes/AvsDec/releases/download/1.0/AvsDec-1.0-Win32.zip", "AvsDec-1.0-Win32.zip" ),
-    ( "https://github.com/wieslawsoltes/AvsDec/releases/download/1.0/AvsDec-1.0-x64.zip", "AvsDec-1.0-x64.zip" ),
+    ( "https://github.com/wieslawsoltes/AvsDec/releases/download/2.2/AvsDec-2.2-Win32-UNICODE.zip", "AvsDec-2.2-Win32-UNICODE.zip" ),
+    ( "https://github.com/wieslawsoltes/AvsDec/releases/download/2.2/AvsDec-2.2-x64-UNICODE.zip", "AvsDec-2.2-x64-UNICODE.zip" ),
     # rewavi
     ( "https://github.com/wieslawsoltes/rewavi/releases/download/0.05/rewavi-0.05-Win32.zip", "rewavi-0.05-Win32.zip" ),
     ( "https://github.com/wieslawsoltes/rewavi/releases/download/0.05/rewavi-0.05-x64.zip", "rewavi-0.05-x64.zip" )
