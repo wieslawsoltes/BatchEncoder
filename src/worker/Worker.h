8db0e9114893dd728812b7eaf1170cbcf82cc932
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2018-01-15 13:45:16 +0100

    Update Worker.h

--- src/worker/Worker.h
+++ src/worker/Worker.h
@@ -20,12 +20,12 @@ public:
     CWorker() { }
     virtual ~CWorker() { }
 public:
-    bool ProgresssLoop(CWorkerContext* pWorkerContext, CFileContext* pContext, CPipe &Stderr, int &nProgress);
-    bool ReadLoop(CWorkerContext* pWorkerContext, CPipeContext* pContext);
-    bool WriteLoop(CWorkerContext* pWorkerContext, CPipeContext* pContext);
-    bool ConvertFileUsingConsole(CWorkerContext* pWorkerContext, CFileContext* pContext);
-    bool ConvertFileUsingPipes(CWorkerContext* pWorkerContext, CFileContext* pContext);
-    bool ConvertFileUsingOnlyPipes(CWorkerContext* pWorkerContext, CFileContext* pDecoderContext, CFileContext* pEncoderContext);
+    bool ProgresssLoop(CWorkerContext* pWorkerContext, CFileContext &context, CPipe &Stderr, int &nProgress);
+    bool ReadLoop(CWorkerContext* pWorkerContext, CPipeContextt &context);
+    bool WriteLoop(CWorkerContext* pWorkerContext, CPipeContextt &context);
+    bool ConvertFileUsingConsole(CWorkerContext* pWorkerContext, CFileContextt &context);
+    bool ConvertFileUsingPipes(CWorkerContext* pWorkerContext, CFileContextt &context);
+    bool ConvertFileUsingOnlyPipes(CWorkerContext* pWorkerContext, CFileContext &decoderContext, CFileContext &encoderContext);
     bool ConvertItem(CWorkerContext* pWorkerContext, CItem& item, CSynchronize &syncDir);
     bool ConvertLoop(CWorkerContext* pWorkerContext, std::queue<CItem> &queue, CSynchronize &sync, CSynchronize &syncDir);
     void Convert(CWorkerContext* pWorkerContext);
