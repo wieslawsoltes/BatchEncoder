141f022c9c13c5ee8b5d7a1528d1d586dec401a9
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2018-01-16 08:52:11 +0100

    Added asserts

--- config/progress/tests.lua
+++ config/progress/tests.lua
@@ -7,6 +7,7 @@ end
 
 p = GetProgress("144200/157023 ( 91%)|  222.6  |   59.6/64.9   |   56.17x | 5.3")
 print(p)
+assert(p == "91")
 
 -- GetProgress_FaadDec
 
@@ -17,8 +18,10 @@ end
 
 p = GetProgress("Decoding 01 Beach House - Myth.aac took:  0.75 sec. 344.92x real-time.")
 print(p)
+assert(p == "100")
 p = GetProgress("31% decoding 01 Beach House - Myth.aac.")
 print(p)
+assert(p == "31")
 
 -- GetProgress_FlacEnc
 
@@ -29,8 +32,10 @@ end
 
 p = GetProgress("Image.wav: wrote 415168867 bytes, ratio=0,646")
 print(p)
+assert(p == "100")
 p = GetProgress("Image.wav: 41% complete, ratio=0,643")
 print(p)
+assert(p == "41")
 
 -- GetProgress_FlacDec
 
@@ -41,8 +46,10 @@ end
 
 p = GetProgress("Image.flac: done")
 print(p)
+assert(p == "100")
 p = GetProgress("Image.flac: 42% complete")
 print(p)
+assert(p == "42")
 
 -- GetProgress_LameEnc
 
@@ -52,6 +59,7 @@ end
 
 p = GetProgress(" 21400/139576 (15%)|    0:09/    0:59|    0:09/    0:59|   61.108x|    0:50")
 print(p)
+assert(p == "15")
 
 -- GetProgress_LameDec
 
@@ -64,13 +72,17 @@ end
 
 p = GetProgress("Frame# 23067/139576 160 kbps   MS")
 print(p)
+assert(p == "16.526480197169")
 p = GetProgress("Frame# 0/0 160 kbps   MS")
 print(p)
+assert(p == "0")
 p = GetProgress("Frame# 0/139576 160 kbps   MS")
 print(p)
+assert(p == "0")
 p = GetProgress("Frame#139576/139576  32 kbps  L  R   ")
 print(p)
- 
+assert(p == "100.0")
+
 --- GetProgress_MacEnc
 
 function GetProgress(s) 
@@ -79,6 +91,7 @@ end
 
 p = GetProgress("Progress: 49.7% (2.8 seconds remaining, 2.7 seconds total)")
 print(p)
+assert(p == "49.7")
 
 --- GetProgress_MacDec
 
@@ -88,6 +101,7 @@ end
 
 p = GetProgress("'Progress: 21.4% (4.8 seconds remaining, 1.3 seconds total)'")
 print(p)
+assert(p == "21.4")
 
 -- GetProgress_TtaEnc
 
@@ -97,6 +111,7 @@ end
 
 p = GetProgress("Progress: 90%")
 print(p)
+assert(p == "90")
 
 -- GetProgress_TtaDec
 
@@ -106,6 +121,7 @@ end
 
 p = GetProgress("Progress: 90%")
 print(p)
+assert(p == "90")
 
 -- GetProgress_WavPackEnc
 
@@ -116,8 +132,10 @@ end
 
 p = GetProgress("created 1.wv in 7.95 secs (lossless, 26.56%)")
 print(p)
+assert(p == "100")
 p = GetProgress("creating 1.wv,  77% done...")
 print(p)
+assert(p == "70")
 
 -- GetProgress_WvUnpackDec
 
@@ -128,8 +146,10 @@ end
 
 p = GetProgress("restored 2.wav in 2.86 secs (lossless, 26.56%)")
 print(p)
+assert(p == "100")
 p = GetProgress("restoring 2.wav,  20% done...")
 print(p)
+assert(p == "20")
 
 -- GetProgress_OggEnc
 
@@ -140,8 +160,10 @@ end
 
 p = GetProgress("[ 22,4%] [ 0m51s remaining] -")
 print(p)
+assert(p == "22,4")
 p = GetProgress("Done encoding file Image.ogg")
 print(p)
+assert(p == "100")
 
 -- GetProgress_OggDec
 
@@ -151,6 +173,7 @@ end
 
 p = GetProgress("   22% decoded.")
 print(p)
+assert(p == "22")
 
 -- GetProgress_MpcEnc
 
@@ -160,8 +183,10 @@ end
 
 p = GetProgress("  -.-    -.- kbps  -.--x     -:--.-    -:--.-     -:--.-    -:--.-     -:--.-")
 print(p)
+assert(p == nil)
 p = GetProgress("  1.5  171.5 kbps 89.11x     0:56.1 1:00:46.0     0:00.6    0:40.9     0:40.2")
 print(p)
+assert(p == "1.5")
 
 -- GetProgress_MpcDec
 
@@ -172,6 +197,7 @@ end
 
 p = GetProgress("160790364 samples decoded in 4350 ms (838.17x)")
 print(p)
+assert(p == "100")
 
 -- GetProgress_OfrEnc
 
@@ -182,8 +208,10 @@ end
 
 p = GetProgress("Compressing   done.")
 print(p)
+assert(p == "100")
 p = GetProgress("Compressing    15.0%")
 print(p)
+assert(p == "15.0")
 
 -- GetProgress_OfrDec
 
@@ -194,8 +222,10 @@ end
 
 p = GetProgress("Decompression done.")
 print(p)
+assert(p == "100")
 p = GetProgress("Decompressing  10.2%")
 print(p)
+assert(p == "10.2")
 
 -- GetProgress_SsrcEnc
 
@@ -205,6 +235,7 @@ end
 
 p = GetProgress("  10% processed")
 print(p)
+assert(p == "10")
 
 -- GetProgress_QaacEnc
 
@@ -215,8 +246,10 @@ end
 
 p = GetProgress("[18.4%] 11:12.333/1:00:46.041 (79.8x), ETA 0:37.250 ")
 print(p)
+assert(p == "18.4")
 p = GetProgress("11406949/11406949 samples processed in 0:03.281")
 print(p)
+assert(p == "100")
 
 -- GetProgress_QaacDec
 
@@ -227,8 +260,10 @@ end
 
 p = GetProgress("[59.9%] 2:34.830/4:18.660 (354.3x), ETA 0:00.293  ")
 print(p)
+assert(p == "59.9")
 p = GetProgress("11406949/11406949 samples processed in 0:00.718")
 print(p)
+assert(p == "100")
 
 -- GetProgress_OpusEnc
 
@@ -239,8 +274,10 @@ end
 
 p = GetProgress("[|] 00:22:45.04 (37%) 75.8x realtime, 193.2kbit/s")
 print(p)
+assert(p == "37")
 p = GetProgress("Encoding complete")
 print(p)
+assert(p == "100")
 
 -- GetProgress_OpusDec
 
@@ -251,8 +288,10 @@ end
 
 p = GetProgress("[/] 00:11:06 (93%)")
 print(p)
+assert(p == "93")
 p = GetProgress("Decoding complete.")
 print(p)
+assert(p == "100")
 
 -- GetProgress_AftenEnc
 
@@ -262,6 +301,7 @@ end
 
 p = GetProgress("progress:  67% | q: 368.9 | bw: 60.0 | bitrate: 448.0 kbps ")
 print(p)
+assert(p == "67")
 
 -- GetProgress_LaEnc
 
@@ -273,12 +313,15 @@ end
 
 p = GetProgress("Encoding 01 Beach House - Myth.wav [........................] (42.4%)")
 print(p)
+assert(p == "-1")
 
 p = GetProgress("Encoding 01 Beach House - Myth.wav [********................] (52.5%)")
 print(p)
+assert(p == "33.333333333333")
 
 p = GetProgress("Encoding 01 Beach House - Myth.wav [************************]")
 print(p)
+assert(p == "100.0")
 
 -- GetProgress_LaDec
 
@@ -290,12 +333,15 @@ end
 
 p = GetProgress("Decoding 01 Beach House - Myth.la [........................]")
 print(p)
+assert(p == "-1")
 
 p = GetProgress("Decoding 01 Beach House - Myth.la [**************..........]")
 print(p)
+assert(p == "58.333333333333")
 
 p = GetProgress("Decoding 01 Beach House - Myth.la [************************]")
 print(p)
+assert(p == "100.0")
 
 -- GetProgress_FFdcaEnc
 
@@ -306,8 +352,10 @@ end
 
 p = GetProgress("Done.")
 print(p)
+assert(p == "100")
 p = GetProgress("Encoding... 3:39 [84.7%]")
 print(p)
+assert(p == "84.7")
 
 -- GetProgress_Eac3toEnc.progress
 
@@ -318,8 +366,10 @@ end
 
 p = GetProgress("Done.")
 print(p)
+assert(p == "100")
 p = GetProgress("process: 88%")
 print(p)
+assert(p == "88")
 
 --- GetProgress_AvsDec
 
@@ -329,3 +379,4 @@ end
 
 p = GetProgress("Progress: 100% (265533250/265533250)")
 print(p)
+assert(p == "100")
