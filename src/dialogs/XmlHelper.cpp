815aa6815ac87d641df4a9d59e61861003458d80
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2018-01-19 12:05:19 +0100

    Create XmlHelper.cpp

---
+++ src/dialogs/XmlHelper.cpp
@@ -0,0 +1,124 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include "StdAfx.h"
+#include "MainApp.h"
+#include "XmlHelper.h"
+
+CXmlHelper::CXmlHelper()
+{
+}
+
+CXmlHelper::~CXmlHelper()
+{
+}
+
+bool CXmlHelper::LoadOptions(XmlDocumnent &doc)
+{
+
+}
+
+bool CXmlHelper::LoadOptions(CString szFileXml)
+{
+
+}
+
+bool CXmlHelper::SaveOptions(CString szFileXml)
+{
+
+}
+
+bool CXmlHelper::LoadItems(XmlDocumnent &doc)
+{
+
+}
+
+bool CXmlHelper::LoadItems(CString szFileXml)
+{
+
+}
+
+bool CXmlHelper::SaveItems(CString szFileXml)
+{
+
+}
+
+bool CXmlHelper::LoadLanguage(XmlDocumnent &doc)
+{
+
+}
+
+bool CXmlHelper::LoadLanguage(CString szFileXml)
+{
+
+}
+
+bool CXmlHelper::LoadPresets(XmlDocumnent &doc)
+{
+
+}
+
+bool CXmlHelper::LoadPresets(CString szFileXml)
+{
+
+}
+
+bool CXmlHelper::SavePresets(CString szFileXml)
+{
+
+}
+
+bool CXmlHelper::LoadFormat(XmlDocumnent &doc)
+{
+
+}
+
+bool CXmlHelper::LoadFormat(CString szFileXml)
+{
+
+}
+
+bool CXmlHelper::SaveFormat(CString szFileXml)
+{
+
+}
+
+bool CXmlHelper::LoadFormats(XmlDocumnent &doc)
+{
+
+}
+
+bool CXmlHelper::LoadFormats(CString szFileXml)
+{
+
+}
+
+bool CXmlHelper::SaveFormats(CString szFileXml)
+{
+
+}
+
+bool CXmlHelper::LoadTool(XmlDocumnent &doc)
+{
+
+}
+
+bool CXmlHelper::LoadTool(CString szFileXml)
+{
+
+}
+
+bool CXmlHelper::LoadTools(XmlDocumnent &doc)
+{
+
+}
+
+bool CXmlHelper::LoadTools(CString szFileXml)
+{
+
+}
+
+bool CXmlHelper::SaveTools(CString szFileXml)
+{
+
+}
