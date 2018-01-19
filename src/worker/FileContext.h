9cbf29fa9ba9361fc94d457acfe40a7b267d753d
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2018-01-20 00:07:55 +0100

    Update FileContext.h

--- src/worker/FileContext.h
+++ src/worker/FileContext.h
@@ -13,6 +13,7 @@ public:
     int nItemId;
     CString szInputFile;
     CString szOutputFile;
+    CString szOutputPath;
     TCHAR pszCommandLine[65536];
     bool bUseReadPipes;
     bool bUseWritePipes;
@@ -21,7 +22,7 @@ public:
     CFileContext() { }
     virtual ~CFileContext() { }
 public:
-    void Init(CFormat *pFormat, int nPreset, int nItemId, CString szInputFile, CString szOutputFile, bool bUseReadPipes, bool bUseWritePipes, CString szOptions)
+    void Init(CFormat *pFormat, int nPreset, int nItemId, CString szInputFile, CString szOutputFile, bool bUseReadPipes, bool bUseWritePipes, CString szAdditionalOptions)
     {
         CPreset& preset = pFormat->m_Presets.Get(nPreset);
 
@@ -32,30 +33,32 @@ public:
         this->szOutputFile = szOutputFile;
         this->bUseReadPipes = bUseReadPipes;
         this->bUseWritePipes = bUseWritePipes;
-        this->szOptions = szOptions.GetLength() > 0 ?
-            (preset.szOptions + _T(" ") + szOptions) : preset.szOptions;
+
+        this->szOptions = szAdditionalOptions.GetLength() > 0 ?
+            (preset.szOptions + _T(" ") + szAdditionalOptions) : preset.szOptions;
 
         CString szCommandLine = pFormat->szTemplate;
 
         szCommandLine = ReplaceNoCase(szCommandLine, _T("$EXE"), _T("\"$EXE\""));
         szCommandLine = ReplaceNoCase(szCommandLine, _T("$EXE"), pFormat->szPath);
-        szCommandLine = ReplaceNoCase(szCommandLine, _T("$OPTIONS"), szOptions);
+        szCommandLine = ReplaceNoCase(szCommandLine, _T("$OPTIONS"), this->szOptions);
 
         szCommandLine = ReplaceNoCase(szCommandLine, _T("$INFILE"), _T("\"$INFILE\""));
         if (bUseReadPipes == true)
             szCommandLine = ReplaceNoCase(szCommandLine, _T("$INFILE"), _T("-"));
         else
-            szCommandLine = ReplaceNoCase(szCommandLine, _T("$INFILE"), szInputFile);
+            szCommandLine = ReplaceNoCase(szCommandLine, _T("$INFILE"), thos->szInputFile);
 
         szCommandLine = ReplaceNoCase(szCommandLine, _T("$OUTFILE"), _T("\"$OUTFILE\""));
         if (bUseWritePipes == true)
             szCommandLine = ReplaceNoCase(szCommandLine, _T("$OUTFILE"), _T("-"));
         else
-            szCommandLine = ReplaceNoCase(szCommandLine, _T("$OUTFILE"), szOutputFile);
+            szCommandLine = ReplaceNoCase(szCommandLine, _T("$OUTFILE"), this->szOutputFile);
 
-        CString szOutputPath = ::GetFilePath(szOutputFile);
+        this->szOutputPath = ::GetFilePath(this->szOutputFile);
+
         szCommandLine = ReplaceNoCase(szCommandLine, _T("$OUTPATH"), _T("\"$OUTPATH\""));
-        szCommandLine = ReplaceNoCase(szCommandLine, _T("$OUTPATH"), szOutputPath);
+        szCommandLine = ReplaceNoCase(szCommandLine, _T("$OUTPATH"), this->szOutputPath);
 
         ZeroMemory(this->pszCommandLine, sizeof(this->pszCommandLine));
         lstrcpy(this->pszCommandLine, szCommandLine.GetBuffer(szCommandLine.GetLength()));
