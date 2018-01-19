8377f3f377b38c0db3a0dd8355bfe98a02189cb4
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2018-01-19 12:38:19 +0100

    Update XmlHelper.cpp

--- src/dialogs/XmlHelper.cpp
+++ src/dialogs/XmlHelper.cpp
@@ -7,12 +7,9 @@
 
 bool CXmlHelper::LoadOptions(XmlDocumnent &doc, COptions &options)
 {
-    XmlDocumnent doc;
-    if (XmlDoc::Open(szFileXml, doc) == true)
-    {
-        return LoadOptions(doc);
-    }
-    return false;
+    XmlOptions xmlOptions(doc);
+    xmlOptions.GetOptions(options);
+    return true;
 }
 
 bool CXmlHelper::LoadOptions(CString szFileXml, COptions &options)
@@ -20,34 +17,24 @@ bool CXmlHelper::LoadOptions(CString szFileXml, COptions &options)
     XmlDocumnent doc;
     if (XmlDoc::Open(szFileXml, doc) == true)
     {
-        return LoadOptions(doc);
+        return LoadOptions(doc, options);
     }
     return false;
 }
 
 bool CXmlHelper::SaveOptions(CString szFileXml, COptions &options)
 {
-    this->GetOptions();
-
     XmlDocumnent doc;
     XmlOptions xmlOptions(doc);
     xmlOptions.Create();
-    xmlOptions.SetOptions(this->m_Config.m_Options);
+    xmlOptions.SetOptions(options);
     return xmlOptions.Save(szFileXml);
 }
 
 bool CXmlHelper::LoadItems(XmlDocumnent &doc, CItemsList &items)
 {
     XmlItems xmlItems(doc);
-
-    this->m_LstInputItems.DeleteAllItems();
-    this->m_Config.m_Items.RemoveAll();
-
-    xmlItems.GetItems(this->m_Config.m_Items);
-
-    this->SetItems();
-    this->UpdateStatusBar();
-
+    xmlItems.GetItems(items);
     return true;
 }
 
@@ -56,44 +43,24 @@ bool CXmlHelper::LoadItems(CString szFileXml, CItemsList &items)
     XmlDocumnent doc;
     if (XmlDoc::Open(szFileXml, doc) == true)
     {
-        return LoadItems(doc);
+        return LoadItems(doc, items);
     }
     return false;
 }
 
 bool CXmlHelper::SaveItems(CString szFileXml, CItemsList &items)
 {
-    this->GetItems();
-
     XmlDocumnent doc;
     XmlItems xmlItems(doc);
     xmlItems.Create();
-    xmlItems.SetItems(this->m_Config.m_Items);
+    xmlItems.SetItems(items);
     return xmlItems.Save(szFileXml);
 }
 
 bool CXmlHelper::LoadLanguage(XmlDocumnent &doc, CLanguage &language)
 {
     XmlLanguages xmlLanguages(doc);
-
-    // insert to languages list
-    CLanguage language;
     xmlLanguages.GetLanguage(language);
-
-    this->m_Config.m_Language.m_Languages.Insert(language);
-
-    // insert to languages menu
-    CMenu *m_hMenu = this->GetMenu();
-    CMenu *m_hLangMenu = m_hMenu->GetSubMenu(4);
-    int nLanguages = m_Config.m_Language.m_Languages.Count();
-
-    CString szBuff;
-    szBuff.Format(_T("%s (%s)"), language.szOriginalName, language.szTranslatedName);
-
-    UINT nLangID = ID_LANGUAGE_MIN + nLanguages - 1;
-    m_hLangMenu->AppendMenu(MF_STRING, nLangID, szBuff);
-    m_hLangMenu->CheckMenuItem(nLangID, MF_UNCHECKED);
-
     return true;
 }
 
@@ -102,28 +69,16 @@ bool CXmlHelper::LoadLanguage(CString szFileXml, CLanguage &language)
     XmlDocumnent doc;
     if (XmlDoc::Open(szFileXml, doc) == true)
     {
-        return LoadLanguage(doc);
+        return LoadLanguage(doc, language);
     }
     return false;
 }
 
 bool CXmlHelper::LoadPresets(XmlDocumnent &doc, CPresetsList &presets)
 {
-    int nFormat = this->m_CmbFormat.GetCurSel();
-    if (nFormat != -1)
-    {
-        CFormat& format = m_Config.m_Formats.Get(nFormat);
-
-        XmlPresets xmlPresets(doc);
-
-        format.m_Presets.RemoveAll();
-        xmlPresets.GetPresets(format.m_Presets);
-
-        this->UpdatePresetComboBox();
-
-        return true;
-    }
-    return false;
+    XmlPresets xmlPresets(doc);
+    xmlPresets.GetPresets(presets);
+    return true;
 }
 
 bool CXmlHelper::LoadPresets(CString szFileXml, CPresetsList &presets)
@@ -131,37 +86,24 @@ bool CXmlHelper::LoadPresets(CString szFileXml, CPresetsList &presets)
     XmlDocumnent doc;
     if (XmlDoc::Open(szFileXml, doc) == true)
     {
-        return LoadPresets(doc);
+        return LoadPresets(doc, presets);
     }
     return false;
 }
 
 bool CXmlHelper::SavePresets(CString szFileXml, CPresetsList &presets)
 {
-    int nFormat = this->m_CmbFormat.GetCurSel();
-    if (nFormat != -1)
-    {
-        CFormat& format = m_Config.m_Formats.Get(nFormat);
-
-        XmlDocumnent doc;
-        XmlPresets xmlPresets(doc);
-        xmlPresets.Create();
-        xmlPresets.SetPresets(format.m_Presets);
-        return xmlPresets.Save(szFileXml);
-    }
-    return false;
+    XmlDocumnent doc;
+    XmlPresets xmlPresets(doc);
+    xmlPresets.Create();
+    xmlPresets.SetPresets(presets;
+    return xmlPresets.Save(szFileXml);
 }
 
 bool CXmlHelper::LoadFormat(XmlDocumnent &doc, CFormat &format)
 {
     XmlFormats xmlFormats(doc);
-
-    CFormat format;
     xmlFormats.GetFormat(format);
-    m_Config.m_Formats.Insert(format);
-
-    this->UpdateFormatComboBox();
-    this->UpdatePresetComboBox();
     return true;
 }
 
@@ -170,38 +112,24 @@ bool CXmlHelper::LoadFormat(CString szFileXml, CFormat &format)
     XmlDocumnent doc;
     if (XmlDoc::Open(szFileXml, doc) == true)
     {
-        return LoadFormat(doc);
+        return LoadFormat(doc, format);
     }
     return false;
 }
 
 bool CXmlHelper::SaveFormat(CString szFileXml, CFormat &format)
 {
-    int nFormat = this->m_CmbFormat.GetCurSel();
-    if (nFormat != -1)
-    {
-        CFormat& format = m_Config.m_Formats.Get(nFormat);
-
-        XmlDocumnent doc;
-        XmlFormats xmlFormats(doc);
-        xmlFormats.Create();
-        xmlFormats.SetFormat(format);
-        return xmlFormats.Save(szFileXml);
-    }
-    return false;
+    XmlDocumnent doc;
+    XmlFormats xmlFormats(doc);
+    xmlFormats.Create();
+    xmlFormats.SetFormat(format);
+    return xmlFormats.Save(szFileXml);
 }
 
 bool CXmlHelper::LoadFormats(XmlDocumnent &doc, CFormatsList &formats)
 {
     XmlFormats xmlFormats(doc);
-
-    this->m_Config.m_Formats.RemoveAll();
-
-    xmlFormats.GetFormats(this->m_Config.m_Formats);
-
-    this->UpdateFormatComboBox();
-    this->UpdatePresetComboBox();
-
+    xmlFormats.GetFormats(formats);
     return true;
 }
 
@@ -210,7 +138,7 @@ bool CXmlHelper::LoadFormats(CString szFileXml, CFormatsList &formats)
     XmlDocumnent doc;
     if (XmlDoc::Open(szFileXml, doc) == true)
     {
-        return LoadFormats(doc);
+        return LoadFormats(doc, formats);
     }
     return false;
 }
@@ -220,18 +148,14 @@ bool CXmlHelper::SaveFormats(CString szFileXml, CFormatsList &formats)
     XmlDocumnent doc;
     XmlFormats xmlFormats(doc);
     xmlFormats.Create();
-    xmlFormats.SetFormats(this->m_Config.m_Formats);
+    xmlFormats.SetFormats(formats);
     return xmlFormats.Save(szFileXml);
 }
 
 bool CXmlHelper::LoadTool(XmlDocumnent &doc, CTool &tool)
 {
     XmlTools xmlTools(doc);
-
-    CTool tool;
     xmlTools.GetTool(tool);
-    m_Config.m_Tools.Insert(tool);
-
     return true;
 }
 
@@ -240,7 +164,7 @@ bool CXmlHelper::LoadTool(CString szFileXml, CTool &tool)
     XmlDocumnent doc;
     if (XmlDoc::Open(szFileXml, doc) == true)
     {
-        return LoadTool(doc);
+        return LoadTool(doc, tool);
     }
     return false;
 }
@@ -257,11 +181,7 @@ bool CXmlHelper::SaveTool(CString szFileXml, CTool &tool)
 bool CXmlHelper::LoadTools(XmlDocumnent &doc, CToolsList &tools)
 {
     XmlTools xmlTools(doc);
-
-    this->m_Config.m_Tools.RemoveAll();
-
-    xmlTools.GetTools(this->m_Config.m_Tools);
-
+    xmlTools.GetTools(tools);
     return true;
 }
 
@@ -270,7 +190,7 @@ bool CXmlHelper::LoadTools(CString szFileXml, CToolsList &tools)
     XmlDocumnent doc;
     if (XmlDoc::Open(szFileXml, doc) == true)
     {
-        return LoadTools(doc);
+        return LoadTools(doc, tools);
     }
     return false;
 }
@@ -280,6 +200,6 @@ bool CXmlHelper::SaveTools(CString szFileXml, CToolsList &tools)
     XmlDocumnent doc;
     XmlTools xmlTools(doc);
     xmlTools.Create();
-    xmlTools.SetTools(this->m_Config.m_Tools);
+    xmlTools.SetTools(tools);
     return xmlTools.Save(szFileXml);
 }
