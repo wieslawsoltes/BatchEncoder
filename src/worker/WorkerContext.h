ed1d16335fb3c261088a1d5b8c67b1d00721f2f0
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2018-01-22 13:53:30 +0100

    Update WorkerContext.h

--- src/worker/WorkerContext.h
+++ src/worker/WorkerContext.h
@@ -5,28 +5,31 @@
 
 #include "configuration\Configuration.h"
 
-class IWorkerContext
+namespace worker
 {
-public:
-    volatile bool bRunning;
-    volatile bool bDone;
-    volatile int nThreadCount;
-    volatile int nTotalFiles;
-    volatile int nProcessedFiles;
-    volatile int nDoneWithoutError;
-    volatile int nErrors;
-    volatile int nLastItemId;
-    config::CConfiguration* pConfig;
-public:
-    IWorkerContext(config::CConfiguration* pConfig)
-        : pConfig(pConfig) { }
-    virtual ~IWorkerContext() { }
-public:
-    virtual void Init() = 0;
-    virtual void Next(int nItemId) = 0;
-    virtual void Done() = 0;
-    virtual bool IsRunning() = 0;
-    virtual bool Callback(int nItemId, int nProgress, bool bFinished, bool bError = false) = 0;
-    virtual void Status(int nItemId, CString szTime, CString szStatus) = 0;
-    virtual CString GetString(int nKey, const TCHAR* szDefault) = 0;
-};
+    class IWorkerContext
+    {
+    public:
+        volatile bool bRunning;
+        volatile bool bDone;
+        volatile int nThreadCount;
+        volatile int nTotalFiles;
+        volatile int nProcessedFiles;
+        volatile int nDoneWithoutError;
+        volatile int nErrors;
+        volatile int nLastItemId;
+    public:
+        config::CConfiguration* pConfig;
+    public:
+        IWorkerContext(config::CConfiguration* pConfig)
+            : pConfig(pConfig) { }
+        virtual ~IWorkerContext() { }
+    public:
+        virtual void Init() = 0;
+        virtual void Next(int nItemId) = 0;
+        virtual void Done() = 0;
+        virtual bool IsRunning() = 0;
+        virtual bool Callback(int nItemId, int nProgress, bool bFinished, bool bError = false) = 0;
+        virtual void Status(int nItemId, CString szTime, CString szStatus) = 0;
+    };
+}
