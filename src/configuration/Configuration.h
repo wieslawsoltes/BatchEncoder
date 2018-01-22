f17a454f7ee1a43bf8868b4dac848367ab1b18a8
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-01-22 20:21:33 +0100

    Added rest of the language strings

--- src/configuration/Configuration.h
+++ src/configuration/Configuration.h
@@ -12,17 +12,187 @@
 
 namespace config
 {
-    const std::map<int, const TCHAR*> m_Strings = 
+    const std::map<int, const TCHAR*> m_Strings =
     {
+        // File Menu
+
+        { 0x00010001, _T("&File") },
+        { 0x00010002, _T("&Load List...\tCtrl+L") },
+        { 0x00010003, _T("&Save List...\tCtrl+S") },
+        { 0x00010004, _T("Cl&ear List\tCtrl+E") },
+        { 0x00010005, _T("E&xit\tAlt+F4") },
+
+        // Edit Menu
+
+        { 0x00020001, _T("&Edit") },
+        { 0x00020002, _T("&Add Files...\tF5") },
+        { 0x00020003, _T("Add Dire&ctory...\tF6") },
+        { 0x00020004, _T("Rena&me\tF2") },
+        { 0x00020005, _T("Rese&t Time/Status\tF3") },
+        { 0x00020006, _T("Reset Output&/Preset\tF4") },
+        { 0x00020007, _T("&Remove\tDEL") },
+        { 0x00020008, _T("Cro&p\tINS") },
+        { 0x00020009, _T("Remove Chec&ked\tCtrl++") },
+        { 0x0002000A, _T("Remove Unc&hecked\tCtrl+-") },
+        { 0x0002000B, _T("Check S&elected\tShift++") },
+        { 0x0002000C, _T("&Uncheck Selected\tShift+-") },
+        { 0x0002000D, _T("&Select All\tCtrl+A") },
+        { 0x0002000E, _T("Select &None\tCtrl+N") },
+        { 0x0002000F, _T("&Invert Selection\tCtrl+I") },
+        { 0x00020010, _T("&Open File...") },
+        { 0x00020011, _T("Open &Directory...") },
+
         // Action Menu
 
+        { 0x00030001, _T("&Action") },
         { 0x00030002, _T("Conve&rt\tF9") },
         { 0x00030003, _T("S&top\tF9") },
 
+        // Options Menu
+
+        { 0x00040001, _T("&Options") },
+        { 0x00040002, _T("Configure &Presets...\tF7") },
+        { 0x00040003, _T("Configure &Formats...\tF8") },
+        { 0x00040004, _T("&Delete Source Files") },
+        { 0x00040005, _T("&Shutdown When Finished") },
+        { 0x00040006, _T("Do Not Save Configuration On E&xit") },
+        { 0x00040007, _T("Delete &Output File On Error") },
+        { 0x00040008, _T("Stop &Conversion On Error") },
+        { 0x00040009, _T("&Hide Console Window") },
+        { 0x0004000A, _T("&Ensure Item Is Visible") },
+        { 0x0004000B, _T("&Try To Find Decoder") },
+        { 0x0004000C, _T("&Validate Input Files") },
+        { 0x0004000D, _T("Ove&rwrite Existing Files") },
+        { 0x0004000E, _T("Configure Too&ls...\tF10") },
+        { 0x0004000F, _T("Tr&y To Download Tools") },
+
+        // Language Menu
+
+        { 0x00050001, _T("&Language") },
+        { 0x00050002, _T("&Default (English)") },
+
+        // Help Menu
+
+        { 0x00060001, _T("&Help") },
+        { 0x00060002, _T("&Website...") },
+        { 0x00060003, _T("&About...") },
+
         // Main Dialog
 
+        { 0x000A0001, _T("Name") },
+        { 0x000A0002, _T("Input") },
+        { 0x000A0003, _T("Size (bytes)") },
+        { 0x000A0004, _T("Output") },
+        { 0x000A0005, _T("Preset#") },
+        { 0x000A0006, _T("Time") },
+        { 0x000A0007, _T("Status") },
+
+        { 0x000A0010, _T("BatchEncoder") },
+        { 0x000A0011, _T("Output") },
+        { 0x000A0012, _T("format:") },
+        { 0x000A0013, _T("preset:") },
+        { 0x000A0014, _T("Output path:") },
+        { 0x000A0015, _T("&...") },
+        { 0x000A0016, _T("threads (0=auto):") },
         { 0x000A0017, _T("Conve&rt") },
         { 0x000A0018, _T("S&top") },
+
+        // Presets Dialog
+
+        { 0x000B0001, _T("Name") },
+        { 0x000B0002, _T("Options") },
+
+        { 0x000B0010, _T("Configure Presets") },
+        { 0x000B0011, _T("&Cancel") },
+        { 0x000B0012, _T("Name:") },
+        { 0x000B0013, _T("Command-line options:") },
+        { 0x000B0014, _T("move &Up") },
+        { 0x000B0015, _T("move Dow&n") },
+        { 0x000B0016, _T("Dele&te All") },
+        { 0x000B0017, _T("&Delete") },
+        { 0x000B0018, _T("&Add") },
+        { 0x000B0019, _T("&Load") },
+        { 0x000B001A, _T("Sa&ve As") },
+        { 0x000B001B, _T("C&hange") },
+        { 0x000B001C, _T("&OK") },
+        { 0x000B001D, _T("Du&plicate") },
+
+        // Formats Dialog
+
+        { 0x000C0001, _T("Name") },
+        { 0x000C0002, _T("Template") },
+
+        { 0x000C0010, _T("Configure Formats") },
+        { 0x000C0011, _T("&Cancel") },
+        { 0x000C0012, _T("Encoder") },
+        { 0x000C0013, _T("Decoder") },
+        { 0x000C0014, _T("Type:") },
+        { 0x000C0015, _T("Input") },
+        { 0x000C0016, _T("Output") },
+        { 0x000C0017, _T("Pipes:") },
+        { 0x000C0018, _T("Id:") },
+        { 0x000C0019, _T("Name:") },
+        { 0x000C001A, _T("Output extension:") },
+        { 0x000C001B, _T("Input extensions (separated by comma):") },
+        { 0x000C001C, _T("Default preset:") },
+        { 0x000C001D, _T("Command-line executable path:") },
+        { 0x000C001E, _T("&...") },
+        { 0x000C001F, _T("Command-line template:") },
+        { 0x000C0020, _T("Progress function:") },
+        { 0x000C0021, _T("...") },
+        { 0x000C0022, _T("move &Up") },
+        { 0x000C0023, _T("move Dow&n") },
+        { 0x000C0024, _T("Dele&te All") },
+        { 0x000C0025, _T("&Delete") },
+        { 0x000C0026, _T("&Add") },
+        { 0x000C0027, _T("&Load") },
+        { 0x000C0028, _T("&Save As") },
+        { 0x000C0029, _T("C&hange") },
+        { 0x000C002A, _T("&OK") },
+        { 0x000C002B, _T("Exit code on success:") },
+        { 0x000C002C, _T("&Edit Presets") },
+        { 0x000C002D, _T("&Import") },
+        { 0x000C002E, _T("E&xport") },
+        { 0x000C002F, _T("Du&plicate") },
+
+        // Tools Dialog
+
+        { 0x000E0001, _T("Name") },
+        { 0x000E0002, _T("Url") },
+        { 0x000E0003, _T("Status") },
+
+        { 0x000E0010, _T("Configure Tools") },
+        { 0x000E0011, _T("&Cancel") },
+        { 0x000E0012, _T("Name:") },
+        { 0x000E0013, _T("Platform:") },
+        { 0x000E0014, _T("Supported formats (separated by comma):") },
+        { 0x000E0015, _T("Download URL:") },
+        { 0x000E0016, _T("Download file name:") },
+        { 0x000E0017, _T("Extract method:") },
+        { 0x000E0018, _T("Extract path:") },
+        { 0x000E0019, _T("move &Up") },
+        { 0x000E001A, _T("move Dow&n") },
+        { 0x000E001B, _T("&Import") },
+        { 0x000E001C, _T("E&xport") },
+        { 0x000E001D, _T("Du&plicate") },
+        { 0x000E001E, _T("Dele&te All") },
+        { 0x000E001F, _T("&Delete") },
+        { 0x000E0020, _T("&Add") },
+        { 0x000E0021, _T("&Load") },
+        { 0x000E0022, _T("&Save As") },
+        { 0x000E0023, _T("Do&wnload Selected") },
+        { 0x000E0024, _T("Stop Do&wnload") },
+        { 0x000E0025, _T("S&et Selected Paths") },
+        { 0x000E0026, _T("Set x&86 Paths") },
+        { 0x000E0027, _T("Set x&64 Paths") },
+        { 0x000E0028, _T("C&hange") },
+        { 0x000E0029, _T("&OK") },
+
+        // About Dialog
+
+        { 0x000D0010, _T("About BatchEncoder") },
+        { 0x000D0011, _T("&OK") },
+        { 0x000D0012, _T("Licensed under the MIT license. See LICENSE file in the application root for full license information.") },
 
         // Strings Work Thread
 
