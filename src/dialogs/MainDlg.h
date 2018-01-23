f2f9f926dba49635ead7a9410ad26e17ecda8ac8
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2018-01-23 11:44:32 +0100

    Update MainDlg.h

--- src/dialogs/MainDlg.h
+++ src/dialogs/MainDlg.h
@@ -98,6 +98,7 @@ namespace app
         afx_msg void OnDropFiles(HDROP hDropInfo);
         afx_msg BOOL OnHelpInfo(HELPINFO* pHelpInfo);
         afx_msg void OnLvnGetdispinfoListItems(NMHDR* pNMHDR, LRESULT* pResult);
+        afx_msg void OnOdfinditemListItems(NMHDR* pNMHDR, LRESULT* pResult);
         afx_msg LRESULT OnListItemChaged(WPARAM wParam, LPARAM lParam);
         afx_msg LRESULT OnNotifyFormat(WPARAM wParam, LPARAM lParam);
         afx_msg void OnNMClickListItems(NMHDR *pNMHDR, LRESULT *pResult);
