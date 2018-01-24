9ff497aef3fa8d6891f4e1a752c8ab438077fe61
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2018-01-24 12:19:31 +0100

    Update XmlConfig.h

--- src/xml/XmlConfig.h
+++ src/xml/XmlConfig.h
@@ -3,8 +3,7 @@
 
 #pragma once
 
-#include <afxwin.h>
-#include <afxcmn.h>
+#include <string>
 #include "XmlOptions.h"
 #include "XmlItems.h"
 #include "XmlLanguages.h"
@@ -17,50 +16,257 @@ namespace xml
     class CXmlConfig
     {
     public:
-        static CString GetRootName(CString szFileXml, XmlDocumnent &doc);
-    public:
-        static bool LoadOptions(XmlDocumnent &doc, config::COptions &options);
-        static bool LoadOptions(CString szFileXml, config::COptions &options);
-        static bool SaveOptions(CString szFileXml, config::COptions &options);
-    public:
-        static bool LoadItem(XmlDocumnent &doc, config::CItem &item);
-        static bool LoadItem(CString szFileXml, config::CItem &item);
-        static bool SaveItem(CString szFileXml, config::CItem &item);
-    public:
-        static bool LoadItems(XmlDocumnent &doc, config::CItemsList &items);
-        static bool LoadItems(CString szFileXml, config::CItemsList &items);
-        static bool SaveItems(CString szFileXml, config::CItemsList &items);
-    public:
-        static bool LoadLanguage(XmlDocumnent &doc, lang::CLanguage &language);
-        static bool LoadLanguage(CString szFileXml, lang::CLanguage &language);
-        static bool SaveLanguage(CString szFileXml, lang::CLanguage &language);
-    public:
-        static bool LoadLanguages(XmlDocumnent &doc, lang::CLanguagesList &languages);
-        static bool LoadLanguages(CString szFileXml, lang::CLanguagesList &languages);
-        static bool SaveLanguages(CString szFileXml, lang::CLanguagesList &languages);
-    public:
-        static bool LoadPreset(XmlDocumnent &doc, config::CPreset &preset);
-        static bool LoadPreset(CString szFileXml, config::CPreset &preset);
-        static bool SavePreset(CString szFileXml, config::CPreset &preset);
-    public:
-        static bool LoadPresets(XmlDocumnent &doc, config::CPresetsList &presets);
-        static bool LoadPresets(CString szFileXml, config::CPresetsList &presets);
-        static bool SavePresets(CString szFileXml, config::CPresetsList &presets);
-    public:
-        static bool LoadFormat(XmlDocumnent &doc, config::CFormat &format);
-        static bool LoadFormat(CString szFileXml, config::CFormat &format);
-        static bool SaveFormat(CString szFileXml, config::CFormat &format);
-    public:
-        static bool LoadFormats(XmlDocumnent &doc, config::CFormatsList &formats);
-        static bool LoadFormats(CString szFileXml, config::CFormatsList &formats);
-        static bool SaveFormats(CString szFileXml, config::CFormatsList &formats);
-    public:
-        static bool LoadTool(XmlDocumnent &doc, config::CTool &tool);
-        static bool LoadTool(CString szFileXml, config::CTool &tool);
-        static bool SaveTool(CString szFileXml, config::CTool &tool);
-    public:
-        static bool LoadTools(XmlDocumnent &doc, config::CToolsList &tools);
-        static bool LoadTools(CString szFileXml, config::CToolsList &tools);
-        static bool SaveTools(CString szFileXml, config::CToolsList &tools);
+        static std::string CXmlConfig::GetRootName(const std::wstring& szFileXml, XmlDocumnent &doc)
+        {
+            if (XmlDoc::Open(szFileXml, doc) == true)
+            {
+                const char *name = XmlDoc::GetRootName(doc);
+                if (name != nullptr)
+                    return std::string(name);
+            }
+            return std::string();
+        }
+        static bool CXmlConfig::LoadOptions(XmlDocumnent &doc, config::COptions &options)
+        {
+            XmlOptions xml(doc);
+            return xml.GetOptions(options);
+        }
+        static bool CXmlConfig::LoadOptions(const std::wstring& szFileXml, config::COptions &options)
+        {
+            XmlDocumnent doc;
+            if (XmlDoc::Open(szFileXml, doc) == true)
+            {
+                return LoadOptions(doc, options);
+            }
+            return false;
+        }
+        static bool CXmlConfig::SaveOptions(const std::wstring& szFileXml, config::COptions &options)
+        {
+            XmlDocumnent doc;
+            XmlOptions xml(doc);
+            xml.Create();
+            xml.SetOptions(options);
+            return xml.Save(szFileXml);
+        }
+        static bool CXmlConfig::LoadItem(XmlDocumnent &doc, config::CItem &item)
+        {
+            XmlItems xml(doc);
+            return xml.GetItem(item);
+        }
+        static bool CXmlConfig::LoadItem(const std::wstring& szFileXml, config::CItem &item)
+        {
+            XmlDocumnent doc;
+            if (XmlDoc::Open(szFileXml, doc) == true)
+            {
+                return LoadItem(doc, item);
+            }
+            return false;
+        }
+        static bool CXmlConfig::SaveItem(const std::wstring& szFileXml, config::CItem &item)
+        {
+            XmlDocumnent doc;
+            XmlItems xml(doc);
+            xml.Create();
+            xml.SetItem(item);
+            return xml.Save(szFileXml);
+        }
+        static bool CXmlConfig::LoadItems(XmlDocumnent &doc, config::CItemsList &items)
+        {
+            XmlItems xml(doc);
+            return xml.GetItems(items);
+        }
+        static bool CXmlConfig::LoadItems(const std::wstring& szFileXml, config::CItemsList &items)
+        {
+            XmlDocumnent doc;
+            if (XmlDoc::Open(szFileXml, doc) == true)
+            {
+                return LoadItems(doc, items);
+            }
+            return false;
+        }
+        static bool CXmlConfig::SaveItems(const std::wstring& szFileXml, config::CItemsList &items)
+        {
+            XmlDocumnent doc;
+            XmlItems xml(doc);
+            xml.Create();
+            xml.SetItems(items);
+            return xml.Save(szFileXml);
+        }
+        static bool CXmlConfig::LoadLanguage(XmlDocumnent &doc, lang::CLanguage &language)
+        {
+            XmlLanguages xml(doc);
+            return xml.GetLanguage(language);
+        }
+        static bool CXmlConfig::LoadLanguage(const std::wstring& szFileXml, lang::CLanguage &language)
+        {
+            XmlDocumnent doc;
+            if (XmlDoc::Open(szFileXml, doc) == true)
+            {
+                return LoadLanguage(doc, language);
+            }
+            return false;
+        }
+        static bool CXmlConfig::SaveLanguage(const std::wstring& szFileXml, lang::CLanguage &language)
+        {
+            XmlDocumnent doc;
+            XmlLanguages xml(doc);
+            xml.Create();
+            xml.SetLanguage(language);
+            return xml.Save(szFileXml);
+        }
+        static bool CXmlConfig::LoadLanguages(XmlDocumnent &doc, lang::CLanguagesList &languages)
+        {
+            XmlLanguages xml(doc);
+            return xml.GetLanguages(languages);
+        }
+        static bool CXmlConfig::LoadLanguages(const std::wstring& szFileXml, lang::CLanguagesList &languages)
+        {
+            XmlDocumnent doc;
+            if (XmlDoc::Open(szFileXml, doc) == true)
+            {
+                return LoadLanguages(doc, languages);
+            }
+            return false;
+        }
+        static bool CXmlConfig::SaveLanguages(const std::wstring& szFileXml, lang::CLanguagesList &languages)
+        {
+            XmlDocumnent doc;
+            XmlLanguages xml(doc);
+            xml.Create();
+            xml.SetLanguages(languages);
+            return xml.Save(szFileXml);
+        }
+        static bool CXmlConfig::LoadPreset(XmlDocumnent &doc, config::CPreset &preset)
+        {
+            XmlPresets xml(doc);
+            return xml.GetPreset(preset);
+        }
+        static bool CXmlConfig::LoadPreset(const std::wstring& szFileXml, config::CPreset &preset)
+        {
+            XmlDocumnent doc;
+            if (XmlDoc::Open(szFileXml, doc) == true)
+            {
+                return LoadPreset(doc, preset);
+            }
+            return false;
+        }
+        static bool CXmlConfig::SavePreset(const std::wstring& szFileXml, config::CPreset &preset)
+        {
+            XmlDocumnent doc;
+            XmlPresets xml(doc);
+            xml.Create();
+            xml.SetPreset(preset);
+            return xml.Save(szFileXml);
+        }
+        static bool CXmlConfig::LoadPresets(XmlDocumnent &doc, config::CPresetsList &presets)
+        {
+            XmlPresets xml(doc);
+            return xml.GetPresets(presets);
+        }
+        static bool CXmlConfig::LoadPresets(const std::wstring& szFileXml, config::CPresetsList &presets)
+        {
+            XmlDocumnent doc;
+            if (XmlDoc::Open(szFileXml, doc) == true)
+            {
+                return LoadPresets(doc, presets);
+            }
+            return false;
+        }
+        static bool CXmlConfig::SavePresets(const std::wstring& szFileXml, config::CPresetsList &presets)
+        {
+            XmlDocumnent doc;
+            XmlPresets xml(doc);
+            xml.Create();
+            xml.SetPresets(presets);
+            return xml.Save(szFileXml);
+        }
+        static bool CXmlConfig::LoadFormat(XmlDocumnent &doc, config::CFormat &format)
+        {
+            XmlFormats xml(doc);
+            return xml.GetFormat(format);
+        }
+        static bool CXmlConfig::LoadFormat(const std::wstring& szFileXml, config::CFormat &format)
+        {
+            XmlDocumnent doc;
+            if (XmlDoc::Open(szFileXml, doc) == true)
+            {
+                return LoadFormat(doc, format);
+            }
+            return false;
+        }
+        static bool CXmlConfig::SaveFormat(const std::wstring& szFileXml, config::CFormat &format)
+        {
+            XmlDocumnent doc;
+            XmlFormats xml(doc);
+            xml.Create();
+            xml.SetFormat(format);
+            return xml.Save(szFileXml);
+        }
+        static bool CXmlConfig::LoadFormats(XmlDocumnent &doc, config::CFormatsList &formats)
+        {
+            XmlFormats xml(doc);
+            return xml.GetFormats(formats);
+        }
+        static bool CXmlConfig::LoadFormats(const std::wstring& szFileXml, config::CFormatsList &formats)
+        {
+            XmlDocumnent doc;
+            if (XmlDoc::Open(szFileXml, doc) == true)
+            {
+                return LoadFormats(doc, formats);
+            }
+            return false;
+        }
+        static bool CXmlConfig::SaveFormats(const std::wstring& szFileXml, config::CFormatsList &formats)
+        {
+            XmlDocumnent doc;
+            XmlFormats xml(doc);
+            xml.Create();
+            xml.SetFormats(formats);
+            return xml.Save(szFileXml);
+        }
+        static bool CXmlConfig::LoadTool(XmlDocumnent &doc, config::CTool &tool)
+        {
+            XmlTools xml(doc);
+            return xml.GetTool(tool);
+        }
+        static bool CXmlConfig::LoadTool(const std::wstring& szFileXml, config::CTool &tool)
+        {
+            XmlDocumnent doc;
+            if (XmlDoc::Open(szFileXml, doc) == true)
+            {
+                return LoadTool(doc, tool);
+            }
+            return false;
+        }
+        static bool CXmlConfig::SaveTool(const std::wstring& szFileXml, config::CTool &tool)
+        {
+            XmlDocumnent doc;
+            XmlTools xml(doc);
+            xml.Create();
+            xml.SetTool(tool);
+            return xml.Save(szFileXml);
+        }
+        static bool CXmlConfig::LoadTools(XmlDocumnent &doc, config::CToolsList &tools)
+        {
+            XmlTools xml(doc);
+            return xml.GetTools(tools);
+        }
+        static bool CXmlConfig::LoadTools(const std::wstring& szFileXml, config::CToolsList &tools)
+        {
+            XmlDocumnent doc;
+            if (XmlDoc::Open(szFileXml, doc) == true)
+            {
+                return LoadTools(doc, tools);
+            }
+            return false;
+        }
+        static bool CXmlConfig::SaveTools(const std::wstring& szFileXml, config::CToolsList &tools)
+        {
+            XmlDocumnent doc;
+            XmlTools xml(doc);
+            xml.Create();
+            xml.SetTools(tools);
+            return xml.Save(szFileXml);
+        }
     };
 }
