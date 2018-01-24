28303093341def56bd980f753fe94e511c628d7a
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2018-01-24 11:46:35 +0100

    Update Format.h

--- src/configuration/Format.h
+++ src/configuration/Format.h
@@ -3,7 +3,8 @@
 
 #pragma once
 
-#include <afxstr.h>
+#include <string>
+#include "StringHelper.h"
 #include "PresetsList.h"
 
 namespace config
@@ -11,67 +12,25 @@ namespace config
     class CFormat
     {
     public:
-        const CString szExtensionsTokens = _T(",");
+        const wchar_t token = ',';
     public:
-        CString szId;
-        CString szName;
-        CString szTemplate;
+        std::wstring szId;
+        std::wstring szName;
+        std::wstring szTemplate;
         bool bPipeInput;
         bool bPipeOutput;
-        CString szFunction;
-        CString szPath;
+        std::wstring szFunction;
+        std::wstring szPath;
         int nExitCodeSuccess;
         int nType; // 0 - Encoder, 1 - Decoder
-        CString szInputExtensions;
-        CString szOutputExtension;
+        std::wstring szInputExtensions;
+        std::wstring szOutputExtension;
         int nDefaultPreset;
         CPresetsList m_Presets;
     public:
-        CFormat()
+        bool IsValidInputExtension(std::wstring& szExt)
         {
-        }
-        CFormat(const CFormat &other)
-        {
-            Copy(other);
-        }
-        CFormat& operator=(const CFormat &other)
-        {
-            Copy(other);
-            return *this;
-        }
-        virtual ~CFormat()
-        {
-        }
-    public:
-        void Copy(const CFormat &other)
-        {
-            this->szId = other.szId;
-            this->szName = other.szName;
-            this->szTemplate = other.szTemplate;
-            this->bPipeInput = other.bPipeInput;
-            this->bPipeOutput = other.bPipeOutput;
-            this->szFunction = other.szFunction;
-            this->szPath = other.szPath;
-            this->nExitCodeSuccess = other.nExitCodeSuccess;
-            this->nType = other.nType;
-            this->szInputExtensions = other.szInputExtensions;
-            this->szOutputExtension = other.szOutputExtension;
-            this->nDefaultPreset = other.nDefaultPreset;
-            this->m_Presets = other.m_Presets;
-        }
-    public:
-        bool IsValidInputExtension(CString szExt)
-        {
-            int nTokenPos = 0;
-            CString strToken = this->szInputExtensions.Tokenize(szExtensionsTokens, nTokenPos);
-            while (!strToken.IsEmpty())
-            {
-                if (strToken.CompareNoCase(szExt) == 0)
-                    return true;
-
-                strToken = this->szInputExtensions.Tokenize(szExtensionsTokens, nTokenPos);
-            }
-            return false;
+            return StringHelper::Contains(this->szInputExtensions, szExt, token);
         }
     };
 }
