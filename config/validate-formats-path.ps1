de791d9f38d98cdaa71d5815937d64af70418f20
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2018-02-06 13:09:30 +0100

    Create validate-formats-path.ps1

---
+++ config/validate-formats-path.ps1
@@ -0,0 +1,14 @@
+﻿# This is BatchEncoder formats validation script for PowerShell.
+# This file was downloaded from https://github.com/wieslawsoltes/BatchEncoder/blob/master/config/validate-formats-path.ps1
+
+$Files = Get-ChildItem "$pwd\formats" -Filter *.xml
+
+"Validate Formats Path:"
+
+ForEach ($File in $Files)
+{
+    [xml]$Xml = Get-Content -Path $File.FullName
+    $path = "$pwd\" + $Xml.Format.path
+    $PathExists = Test-Path $path
+    if ($PathExists) { '[OK] ' + $path } else { '[X] ' + $path }
+}
