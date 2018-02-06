1220c32403e991bc169ff87b8383f8e3d2004ddd
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2018-02-06 12:24:18 +0100

    Update validate-tools-path.ps1

--- config/validate-tools-path.ps1
+++ config/validate-tools-path.ps1
@@ -1,16 +1,14 @@
 ﻿# This is BatchEncoder tools validation script for PowerShell.
 # This file was downloaded from https://github.com/wieslawsoltes/BatchEncoder/blob/master/config/validate-tools-path.ps1
 
-$PathTools = 'Tools.xml'
-[xml]$Xml = Get-Content -Path $PathTools
+$Files = Get-ChildItem "$pwd\tools" -Filter *.xml
 
 "Validate Tools Path:"
 
-foreach ($Tool in $Xml.Tools.Tool) 
+ForEach ($File in $Files)
 {
-    $XmlPath = "$pwd\tools\" + $Tool.name + ".xml"
-    [xml]$XmlTool = Get-Content -Path $XmlPath
-    $path = "$pwd\tools\" + $XmlTool.Tool.path
+    [xml]$Xml = Get-Content -Path $File.FullName
+    $path = "$pwd\tools\" + $Xml.Tool.path
     $PathExists = Test-Path $path
     if ($PathExists) { '[OK] ' + $path } else { '[X] ' + $path }
 }
