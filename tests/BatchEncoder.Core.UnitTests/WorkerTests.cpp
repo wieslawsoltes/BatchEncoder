d7a0b3107a38cf928030ca30537787725bfd3575
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-02-28 23:31:06 +0100

    Added convert nothing test

--- tests/BatchEncoder.Core.UnitTests/WorkerTests.cpp
+++ tests/BatchEncoder.Core.UnitTests/WorkerTests.cpp
@@ -73,12 +73,37 @@ namespace BatchEncoderCoreUnitTests
     TEST_CLASS(CWorker_Tests)
     {
     public:
-        TEST_METHOD(CWorker_Constructor)
+        TEST_METHOD(CWorker_Convert_Nothing)
         {
-            #pragma warning(push)
-            #pragma warning(disable:4101)
+            config::CConfiguration m_Config;
+
+            CTestWorkerContext ctx;
+            Assert::IsTrue(ctx.bDone);
+            Assert::IsNull(ctx.pConfig);
+
+            ctx.bRunning = true;
+            ctx.bDone = false;
+            ctx.nTotalFiles = 0;
+            ctx.nProcessedFiles = 0;
+            ctx.nErrors = 0;
+            ctx.nLastItemId = -1;
+            ctx.nThreadCount = m_Config.m_Options.nThreadCount;
+            ctx.pConfig = &m_Config;
+
+            Assert::AreEqual(0, ctx.nThreadCount);
+            Assert::IsNotNull(ctx.pConfig);
+
             worker::CWorker m_Worker;
-            #pragma warning(pop)
+            m_Worker.Convert(&ctx);
+
+            Assert::IsFalse(ctx.bRunning);
+            Assert::IsTrue(ctx.bDone);
+            Assert::IsTrue(ctx.nTotalFiles == 0);
+            Assert::IsTrue(ctx.nProcessedFiles == 0);
+            Assert::IsTrue(ctx.nErrors == 0);
+            Assert::IsTrue(ctx.nLastItemId == -1);
+            Assert::AreEqual(0, ctx.nThreadCount);
+            Assert::IsNull(ctx.pConfig);
         }
     };
 }
