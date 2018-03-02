91893162bebf3b9a6d9953a04fbd99b436745c38
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-03-02 12:00:26 +0000

    Updated new output path class

--- tests/BatchEncoder.Core.UnitTests/worker/NewOutputPathTests.cpp
+++ tests/BatchEncoder.Core.UnitTests/worker/NewOutputPathTests.cpp
@@ -9,6 +9,18 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+#define VAR_INPUT_DRIVE         L"$InputDrive$"
+#define VAR_INPUT_DIR           L"$InputDir$"
+#define VAR_INPUT_NAME          L"$InputName$"
+#define VAR_INPUT_EXT           L"$InputExt$"
+#define VAR_INPUT_PATH          L"$InputPath$"
+#define VAR_INPUT_FOLDER_INDEX  L"index"
+#define VAR_INPUT_FOLDER        L"$InputFolder[index]$"
+#define VAR_OUTPUT_NAME         L"$Name$"
+#define VAR_OUTPUT_EXT          L"$Ext$"
+#define VAR_OUTPUT_FULL         L"$InputPath$\\$Name$.$Ext$"
+#define VAR_OUTPUT_RELATIVE     L"$Name$.$Ext$"
+
 namespace BatchEncoderCoreUnitTests
 {
     class CNewOutputPath
@@ -24,45 +36,47 @@ namespace BatchEncoderCoreUnitTests
             if (szOutputFile.empty())
             {
                 // empty string
-                szOutputFile = L"$InputPath$\\$Name$.$Ext$";
+                szOutputFile = VAR_OUTPUT_FULL;
             }
-            else if (szOutputFile == L"$InputPath$")
+            else if (szOutputFile == VAR_INPUT_PATH)
             {
                 // input path only
-                szOutputFile += L"\\$Name$.$Ext$";
+                szOutputFile += L"\\";
+                szOutputFile += VAR_OUTPUT_RELATIVE;
             }
             else
             {
-                // no variables are present and string is not empty e.g.: "C:\Output" or "C:\Output\"
+                // no variables are present and string is not empty e.g.: "C:\Output", "C:\Output\" or relative path
                 if (szOutputFile.find('$') == std::wstring::npos)
                 {
                     wchar_t szValidPath[_MAX_PATH];
                     _wmakepath_s(szValidPath, nullptr, szOutputFile.c_str(), nullptr, nullptr);
                     szOutputFile = szValidPath;
-                    szOutputFile += L"$Name$.$Ext$";
+                    szOutputFile += VAR_OUTPUT_RELATIVE;
                 }
             }
 
             // replace custom name
-            util::StringHelper::ReplaceNoCase(szOutputFile, L"$Name$", szName);
+            util::StringHelper::ReplaceNoCase(szOutputFile, VAR_OUTPUT_NAME, szName);
 
             // replace custom ext
-            util::StringHelper::ReplaceNoCase(szOutputFile, L"$Ext$", util::StringHelper::TowLower(szExt));
+            util::StringHelper::ReplaceNoCase(szOutputFile, VAR_OUTPUT_EXT, util::StringHelper::TowLower(szExt));
 
             // replace input path parts
-            util::StringHelper::ReplaceNoCase(szOutputFile, L"$InputDrive$", m_Input.szInputDrive);
-            util::StringHelper::ReplaceNoCase(szOutputFile, L"$InputDir$", m_Input.szInputDir);
-            util::StringHelper::ReplaceNoCase(szOutputFile, L"$InputName$", m_Input.szInputName);
-            util::StringHelper::ReplaceNoCase(szOutputFile, L"$InputExt$", util::StringHelper::TowLower(m_Input.szInputExt));
-            util::StringHelper::ReplaceNoCase(szOutputFile, L"$InputPath$", m_Input.szInputPath);
+            util::StringHelper::ReplaceNoCase(szOutputFile, VAR_INPUT_DRIVE, m_Input.szInputDrive);
+            util::StringHelper::ReplaceNoCase(szOutputFile, VAR_INPUT_DIR, m_Input.szInputDir);
+            util::StringHelper::ReplaceNoCase(szOutputFile, VAR_INPUT_NAME, m_Input.szInputName);
+            util::StringHelper::ReplaceNoCase(szOutputFile, VAR_INPUT_EXT, util::StringHelper::TowLower(m_Input.szInputExt));
+            util::StringHelper::ReplaceNoCase(szOutputFile, VAR_INPUT_PATH, m_Input.szInputPath);
 
-            // replace input path folders $InputFolder[index]$
+            // replace input path folders
             size_t nFoldersSize = m_Input.szSourceFolders.size();
             for (size_t i = nFoldersSize; i > 0; i--)
             {
                 std::wstring& folder = m_Input.szSourceFolders[i - 1];
                 std::wstring szVarIndex = std::to_wstring(nFoldersSize - i);
-                std::wstring szVarName = L"$InputFolder[" + szVarIndex + L"]$";
+                std::wstring szVarName = VAR_INPUT_FOLDER;
+                util::StringHelper::Replace(szVarName, VAR_INPUT_FOLDER_INDEX, szVarIndex);
                 util::StringHelper::ReplaceNoCase(szOutputFile, szVarName, folder);
             }
 
@@ -101,6 +115,24 @@ namespace BatchEncoderCoreUnitTests
             std::wstring szOutputFile = m_Output.CreateFilePath(szOutput, szInputFile, szName, szExt);
 
             Assert::AreEqual(L"C:\\MusicFolder\\ArtistFolder\\AlbumFolder\\CustomName.ext", szOutputFile.c_str());
+        }
+
+        TEST_METHOD(CNewOutputPath_CreateFile_Relative)
+        {
+            std::wstring szOutput = L"relative\\path";
+            std::wstring szInputFile = L"C:\\MusicFolder\\ArtistFolder\\AlbumFolder\\FileName.wav";
+            std::wstring szName = L"CustomName";
+            std::wstring szExt = L"ext";
+
+            CNewOutputPath m_Output;
+            std::wstring szOutputFile = m_Output.CreateFilePath(szOutput, szInputFile, szName, szExt);
+
+            wchar_t szCurrentDirectory[_MAX_PATH];
+            ::GetCurrentDirectory(_MAX_PATH, szCurrentDirectory);
+            std::wstring szExpectedOutputFile = szCurrentDirectory;
+            szExpectedOutputFile += L"\\" + szOutput + L"\\CustomName.ext";
+
+            Assert::AreEqual(szExpectedOutputFile.c_str(), szOutputFile.c_str());
         }
 
         TEST_METHOD(CNewOutputPath_CreateFile_NoVariables_NoSlash)
@@ -145,6 +177,19 @@ namespace BatchEncoderCoreUnitTests
         TEST_METHOD(CNewOutputPath_CreateFilePath_InputPath_InputName_Ext)
         {
             std::wstring szOutput = L"$InputPath$\\$InputName$.$Ext$";
+            std::wstring szInputFile = L"C:\\MusicFolder\\ArtistFolder\\AlbumFolder\\FileName.wav";
+            std::wstring szName = L"CustomName";
+            std::wstring szExt = L"ext";
+
+            CNewOutputPath m_Output;
+            std::wstring szOutputFile = m_Output.CreateFilePath(szOutput, szInputFile, szName, szExt);
+
+            Assert::AreEqual(L"C:\\MusicFolder\\ArtistFolder\\AlbumFolder\\FileName.ext", szOutputFile.c_str());
+        }
+
+        TEST_METHOD(CNewOutputPath_CreateFilePath_InputPath_InputName_Ext_NoCase)
+        {
+            std::wstring szOutput = L"$inputpath$\\$inputname$.$ext$";
             std::wstring szInputFile = L"C:\\MusicFolder\\ArtistFolder\\AlbumFolder\\FileName.wav";
             std::wstring szName = L"CustomName";
             std::wstring szExt = L"ext";
