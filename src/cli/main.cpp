8ccc6c3453e465a266a40ade46569218f4c2aa64
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-03-04 17:44:56 +0100

    Added cli project

---
+++ src/cli/main.cpp
@@ -0,0 +1,10 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <SDKDDKVer.h>
+#include <Windows.h>
+#include "mainapp.h"
+#include "utilities\ArgvParser.h"
+
+int wmain(int argc, wchar_t *argv[])
+{
+    return 0;
+}
