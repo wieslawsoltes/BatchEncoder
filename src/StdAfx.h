f16315d60e37e8e7e592ce1396f2ca1903b91d00
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-03-06 10:38:55 +0000

    Optimize stdafx

--- src/StdAfx.h
+++ src/StdAfx.h
@@ -7,6 +7,8 @@
 #define _SECURE_ATL 1
 #endif
 
+#define WIN32_LEAN_AND_MEAN
+
 #ifndef VC_EXTRALEAN
 #define VC_EXTRALEAN
 #endif
@@ -23,6 +25,8 @@
 #define _AFX_NO_OLEDB_SUPPORT
 #define _AFX_NO_RICHEDIT_SUPPORT
 
+#define _AFX_NO_MFC_CONTROLS_IN_DIALOGS
+
 #include <afxwin.h>
 #include <afxext.h>
 #include <afxdisp.h>
@@ -33,8 +37,6 @@
 #ifndef _AFX_NO_AFXCMN_SUPPORT
 #include <afxcmn.h>
 #endif
-
-#include <afxcontrolbars.h> 
 
 #ifdef _UNICODE
 #if defined _M_IX86
