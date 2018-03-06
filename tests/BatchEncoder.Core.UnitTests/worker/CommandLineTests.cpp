7e2569b6905b6094e8a50e7b43f83d1035fca0da
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-03-06 19:51:34 +0100

    Added worker command line tests

--- tests/BatchEncoder.Core.UnitTests/worker/CommandLineTests.cpp
+++ tests/BatchEncoder.Core.UnitTests/worker/CommandLineTests.cpp
@@ -9,13 +9,200 @@ namespace BatchEncoderCoreUnitTests
 {
     TEST_CLASS(CCommandLine_Tests)
     {
-    public:
-        TEST_METHOD(CCommandLine_Constructor)
+        config::CFormat m_Format
         {
-            #pragma warning(push)
-            #pragma warning(disable:4101)
-            worker::CCommandLine m_CommandLine;
-            #pragma warning(pop)
+            L"TEST_ID",
+            L"Name",
+            L"$EXE $OPTIONS $INFILE $OUTFILE",
+            false,
+            false,
+            L"script.lua",
+            L"program.exe",
+            0,
+            config::FormatType::Encoder,
+            0,
+            L"WAV",
+            L"MP3",
+            0,
+            {
+                { L"Default1", L"--option 1" },
+                { L"Default2", L"--option 2" }
+            }
+        };
+        config::CItem m_ltItem
+        {
+            0,
+            {
+                { L"C:\\Output\\File.WAV" }
+            },
+            1024,
+            L"File",
+            L"WAV",
+            L"TEST_ID",
+            0,
+            L"",
+            true,
+            L"", L"", false, 0, 0
+        };
+    public:
+        TEST_METHOD(CCommandLine_Build_Default)
+        {
+            config::CFormat format = m_Format;
+            config::CItem item = m_ltItem;
+
+            std::wstring szInputFile = L"C:\\Output\\File.WAV";
+            std::wstring szOutputFile = L"C:\\Output\\File.MP3";
+
+            worker::CCommandLine cl;
+            cl.Build(&format, item.nPreset, item.nId, szInputFile, szOutputFile, format.bPipeInput, format.bPipeOutput, item.szOptions);
+
+            Assert::IsTrue(&format == cl.format);
+            Assert::AreEqual(item.nPreset, cl.nPreset);
+            Assert::AreEqual(szInputFile, cl.szInputFile);
+            Assert::AreEqual(szOutputFile, cl.szOutputFile);
+            Assert::AreEqual(format.bPipeInput, cl.bUseReadPipes);
+            Assert::AreEqual(format.bPipeOutput, cl.bUseWritePipes);
+            Assert::AreEqual(format.m_Presets[item.nPreset].szOptions, cl.szOptions);
+            Assert::AreEqual(L"C:\\Output\\", cl.szOutputPath.c_str());
+
+            std::wstring szCommandLine = L"\"program.exe\" --option 1 \"C:\\Output\\File.WAV\" \"C:\\Output\\File.MP3\"";
+            Assert::AreEqual(szCommandLine.c_str(), cl.szCommandLine.c_str());
+        }
+
+        TEST_METHOD(CCommandLine_Build_AdditionalOptions)
+        {
+            config::CFormat format = m_Format;
+            config::CItem item = m_ltItem;
+
+            std::wstring szInputFile = L"C:\\Output\\File.WAV";
+            std::wstring szOutputFile = L"C:\\Output\\File.MP3";
+
+            item.szOptions = L"--test";
+
+            worker::CCommandLine cl;
+            cl.Build(&format, item.nPreset, item.nId, szInputFile, szOutputFile, format.bPipeInput, format.bPipeOutput, item.szOptions);
+
+            Assert::IsTrue(&format == cl.format);
+            Assert::AreEqual(item.nPreset, cl.nPreset);
+            Assert::AreEqual(szInputFile, cl.szInputFile);
+            Assert::AreEqual(szOutputFile, cl.szOutputFile);
+            Assert::AreEqual(format.bPipeInput, cl.bUseReadPipes);
+            Assert::AreEqual(format.bPipeOutput, cl.bUseWritePipes);
+
+            std::wstring szOptions = format.m_Presets[item.nPreset].szOptions + L" " + item.szOptions;
+            Assert::AreEqual(szOptions, cl.szOptions);
+
+            Assert::AreEqual(L"C:\\Output\\", cl.szOutputPath.c_str());
+
+            std::wstring szCommandLine = L"\"program.exe\" --option 1 --test \"C:\\Output\\File.WAV\" \"C:\\Output\\File.MP3\"";
+            Assert::AreEqual(szCommandLine.c_str(), cl.szCommandLine.c_str());
+        }
+
+        TEST_METHOD(CCommandLine_Build_OutPath)
+        {
+            config::CFormat format = m_Format;
+            config::CItem item = m_ltItem;
+
+            std::wstring szInputFile = L"C:\\Output\\File.WAV";
+            std::wstring szOutputFile = L"C:\\Output\\File.MP3";
+
+            format.szTemplate = L"$EXE $OPTIONS $INFILE $OUTFILE $OUTPATH";
+
+            worker::CCommandLine cl;
+            cl.Build(&format, item.nPreset, item.nId, szInputFile, szOutputFile, format.bPipeInput, format.bPipeOutput, item.szOptions);
+
+            Assert::IsTrue(&format == cl.format);
+            Assert::AreEqual(item.nPreset, cl.nPreset);
+            Assert::AreEqual(szInputFile, cl.szInputFile);
+            Assert::AreEqual(szOutputFile, cl.szOutputFile);
+            Assert::AreEqual(format.bPipeInput, cl.bUseReadPipes);
+            Assert::AreEqual(format.bPipeOutput, cl.bUseWritePipes);
+            Assert::AreEqual(format.m_Presets[item.nPreset].szOptions, cl.szOptions);
+            Assert::AreEqual(L"C:\\Output\\", cl.szOutputPath.c_str());
+
+            std::wstring szCommandLine = L"\"program.exe\" --option 1 \"C:\\Output\\File.WAV\" \"C:\\Output\\File.MP3\" \"C:\\Output\\\"";
+            Assert::AreEqual(szCommandLine.c_str(), cl.szCommandLine.c_str());
+        }
+
+        TEST_METHOD(CCommandLine_Build_UseReadPipes)
+        {
+            config::CFormat format = m_Format;
+            config::CItem item = m_ltItem;
+
+            format.bPipeInput = true;
+            format.bPipeOutput = false;
+
+            std::wstring szInputFile = L"C:\\Output\\File.WAV";
+            std::wstring szOutputFile = L"C:\\Output\\File.MP3";
+
+            worker::CCommandLine cl;
+            cl.Build(&format, item.nPreset, item.nId, szInputFile, szOutputFile, format.bPipeInput, format.bPipeOutput, item.szOptions);
+
+            Assert::IsTrue(&format == cl.format);
+            Assert::AreEqual(item.nPreset, cl.nPreset);
+            Assert::AreEqual(szInputFile, cl.szInputFile);
+            Assert::AreEqual(szOutputFile, cl.szOutputFile);
+            Assert::AreEqual(format.bPipeInput, cl.bUseReadPipes);
+            Assert::AreEqual(format.bPipeOutput, cl.bUseWritePipes);
+            Assert::AreEqual(format.m_Presets[item.nPreset].szOptions, cl.szOptions);
+            Assert::AreEqual(L"C:\\Output\\", cl.szOutputPath.c_str());
+
+            std::wstring szCommandLine = L"\"program.exe\" --option 1 \"-\" \"C:\\Output\\File.MP3\"";
+            Assert::AreEqual(szCommandLine.c_str(), cl.szCommandLine.c_str());
+        }
+
+        TEST_METHOD(CCommandLine_Build_UseWritePipes)
+        {
+            config::CFormat format = m_Format;
+            config::CItem item = m_ltItem;
+
+            format.bPipeInput = false;
+            format.bPipeOutput = true;
+
+            std::wstring szInputFile = L"C:\\Output\\File.WAV";
+            std::wstring szOutputFile = L"C:\\Output\\File.MP3";
+
+            worker::CCommandLine cl;
+            cl.Build(&format, item.nPreset, item.nId, szInputFile, szOutputFile, format.bPipeInput, format.bPipeOutput, item.szOptions);
+
+            Assert::IsTrue(&format == cl.format);
+            Assert::AreEqual(item.nPreset, cl.nPreset);
+            Assert::AreEqual(szInputFile, cl.szInputFile);
+            Assert::AreEqual(szOutputFile, cl.szOutputFile);
+            Assert::AreEqual(format.bPipeInput, cl.bUseReadPipes);
+            Assert::AreEqual(format.bPipeOutput, cl.bUseWritePipes);
+            Assert::AreEqual(format.m_Presets[item.nPreset].szOptions, cl.szOptions);
+            Assert::AreEqual(L"C:\\Output\\", cl.szOutputPath.c_str());
+
+            std::wstring szCommandLine = L"\"program.exe\" --option 1 \"C:\\Output\\File.WAV\" \"-\"";
+            Assert::AreEqual(szCommandLine.c_str(), cl.szCommandLine.c_str());
+        }
+
+        TEST_METHOD(CCommandLine_Build_UseReadWritePipes)
+        {
+            config::CFormat format = m_Format;
+            config::CItem item = m_ltItem;
+
+            format.bPipeInput = true;
+            format.bPipeOutput = true;
+
+            std::wstring szInputFile = L"C:\\Output\\File.WAV";
+            std::wstring szOutputFile = L"C:\\Output\\File.MP3";
+
+            worker::CCommandLine cl;
+            cl.Build(&format, item.nPreset, item.nId, szInputFile, szOutputFile, format.bPipeInput, format.bPipeOutput, item.szOptions);
+
+            Assert::IsTrue(&format == cl.format);
+            Assert::AreEqual(item.nPreset, cl.nPreset);
+            Assert::AreEqual(szInputFile, cl.szInputFile);
+            Assert::AreEqual(szOutputFile, cl.szOutputFile);
+            Assert::AreEqual(format.bPipeInput, cl.bUseReadPipes);
+            Assert::AreEqual(format.bPipeOutput, cl.bUseWritePipes);
+            Assert::AreEqual(format.m_Presets[item.nPreset].szOptions, cl.szOptions);
+            Assert::AreEqual(L"C:\\Output\\", cl.szOutputPath.c_str());
+
+            std::wstring szCommandLine = L"\"program.exe\" --option 1 \"-\" \"-\"";
+            Assert::AreEqual(szCommandLine.c_str(), cl.szCommandLine.c_str());
         }
     };
 }
